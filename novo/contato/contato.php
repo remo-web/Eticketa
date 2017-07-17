bee0ea5f0d6527382f4587cb32850598fa5f8862
Raphael <raphaelrpais@gmail.com>
2017-07-17 17:52:21 -0300

    contato

--- novo/contato/contato.php
+++ novo/contato/contato.php
@@ -8,7 +8,7 @@ if(empty($_POST['name'])  		||
    empty($_POST['message'])	    ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "No arguments Provided!";
+	echo "";
 	return false;
    }
 

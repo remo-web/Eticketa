dff2b0357a730d143a2192d0f1f7322a2e81db6c
Raphael <raphaelrpais@gmail.com>
2017-07-31 23:57:53 -0300

    surra de php

--- novo/contato/contato.php
+++ novo/contato/contato.php
@@ -1,53 +1,55 @@
 <?php
-// check if fields passed are empty
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['company'])     ||
-   empty($_POST['phone']) 		||
-   empty($_POST['subject']) 	||
-   empty($_POST['message'])	    ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "";
-	return false;
-   }
+// define variables and set to empty values
+$nomeErr = $emailErr = $assuntoErr = $mensagemErr = "";
+$nome = $email = $empresa = $tel = $assunto = $mensagem = "";
 
-if ($_SERVER['REQUEST_METHOD'] == "POST"){
-    // The form has been submitted
-    echo '
-        <span class="mdl-chip mdl-chip--contact">
-            <span class="mdl-chip__contact mdl-color--green mdl-color-text--white">
-                <i class="material-icons" style="vertical-align: middle">done</i>
-            </span>
-            <span class="mdl-chip__text">Mensagem enviada!</span>
-        </span>
-    ';
-  } else {
-    echo '
-        <span class="mdl-chip mdl-chip--contact">
-            <span class="mdl-chip__contact mdl-color--red mdl-color-text--white">
-                <i class="material-icons" style="vertical-align: middle">close</i>
-            </span>
-            <span class="mdl-chip__text">Ops! Mensagem não enviada, por favor tente novamente.</span>
-        </span>
-    ';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    if (empty($_POST["contato-nome"])) {
+        $nomeErr = "Name is required";
+    } else {
+        $nome = test_input($_POST["contato-nome"]);
+    }
+    
+    if (empty($_POST["contato-email"])) {
+        $emailErr = "Email is required";
+    } else {
+        $email = test_input($_POST["contato-email"]);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format"; 
+        }
+    }
+    
+    if (empty($_POST["contato-empresa"])) {
+        $empresa = "";
+    } else {
+        $empresa = test_input($_POST["contato-empresa"]);
+    }
+    
+    if (empty($_POST["contato-tel"])) {
+        $tel = "";
+    } else {
+        $tel = test_input($_POST["contato-tel"]);
+    }
+    
+    if (empty($_POST["contato-assunto"])) {
+        $assuntoErr = "Assunto is required";
+    } else {
+        $assunto = test_input($_POST["contato-assunto"]);
+    }
+    
+    if (empty($_POST["contato-mensagem"])) {
+        $mensagemErr = "Mensagem is required";
+    } else {
+        $mensagem = test_input($_POST["contato-mensagem"]);
+    }
 }
-	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$company = $_POST['company'];
-$phone = $_POST['phone'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-	
-// create email body and send it
-$to = "raphael.pais@eticketa.com.br"; // *REPLACE WITH THE EMAIL ADDRESS YOU WANT THE FORM TO SEND MAIL TO* TO BE FROM*
-$headers = "From: raphael.pais@eticketa.com.br\n"; // *REPLACE WITH THE EMAIL ADDRESS YOU WANT THE MESSAGE
-$headers = "MIME-Version: 1.0";
-$headers = "Content-type: text/html; charset=UTF-8";
-$headers = "Reply-To: $email_address";		
-$email_subject = "[SITE | Contato] $subject";
-$email_body = "You have received a new message from your website contact form.\n\nHere are the details:\n\nName: $name\n\nCargo / Empresa: $company\n\nE-mail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 ?>

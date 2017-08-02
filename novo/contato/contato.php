35c8ba0adcb4aa271489646c93cae6afd7c5319e
Raphael <raphaelrpais@gmail.com>
2017-08-02 01:51:01 -0300

    será?

--- novo/contato/contato.php
+++ novo/contato/contato.php
@@ -55,7 +55,7 @@ $headers .= 'Content-Transfer-Encoding: 8bit' . "\r\n";
 $headers .= 'Content-Type: text/plain; charset=UTF-8' . "\r\n";
  
 // send email
-$success = mail($To, $Subject, $Body, "From:".$email, $headers);
+$success = mail($To, $Subject, $Body, $headers, "From:".$email);
  
 // redirect to success page
 if ($success && $error == ""){

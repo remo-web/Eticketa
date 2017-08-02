3f8a31f31fcc8890dd954c24c39463635bc4f8ce
Raphael <raphaelrpais@gmail.com>
2017-08-02 11:54:52 -0300

    headers-from$email

--- novo/contato/contato.php
+++ novo/contato/contato.php
@@ -50,12 +50,13 @@ $Body .= "Mensagem: ";
 $Body .= $mensagem;
 $Body .= "\n";
 
-$headers = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-Transfer-Encoding: 8bit' . "\r\n";
-$headers .= 'Content-Type: text/html; charset=UTF-8' . "\r\n";
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8" . "\r\n";
+$headers .= "From: $email" . "\r\n";
  
 // send email
-$success = mail($To, $Subject, $Body, $headers, "From:".$email);
+$success = mail($To, $Subject, $Body, $headers);
  
 // redirect to success page
 if ($success && $error == ""){

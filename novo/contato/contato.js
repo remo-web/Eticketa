fb3047a1b69b61662f006233c414f4f0f1112654
Raphael <raphaelrpais@gmail.com>
2017-08-02 17:02:41 -0300

    a

--- novo/contato/contato.js
+++ novo/contato/contato.js
@@ -12,7 +12,7 @@ $("#etka_contato").submit(function(event){
 
 //VALIDACAO
 function contatoValidar() {
-    var email = document.forms["#setka_contato"]["#contato-email"].value;
+    var email = document.forms["#etka_contato"]["#contato-email"].value;
     if (email == "") {
         alert("Preencha o seu e-mail, assim lhe antenderemos mais rapidamente.");
         return false;

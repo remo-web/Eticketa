67a5a217f4d97a320ee1be8babf71a983ca2123d
Raphael Pais <oproprio@raphaelrpais.com>
2017-08-09 16:21:13 -0300

    function again

--- novo/contato/contato.js
+++ novo/contato/contato.js
@@ -1,3 +1,4 @@
+function submitContato(){
 $("#etka_contato").submit(function(event){
     var email = document.forms["etka_contato"]["contato-email"].value;
     var contato_email = document.getElementById("contato-email");
@@ -25,6 +26,7 @@ $("#etka_contato").submit(function(event){
         submitForm();
     }
 });
+}
 
 function submitForm(){
     // Initiate Variables With Form Content

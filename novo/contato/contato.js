2cb3794507b5cba4079b2a475ddd97da0130fdd4
Raphael Pais <oproprio@raphaelrpais.com>
2017-09-05 17:52:04 -0300

    séerio?

--- novo/contato/contato.js
+++ novo/contato/contato.js
@@ -31,14 +31,14 @@ function submitContato(){
     var nome = $("#contato-nome").val();
     var email = $("#contato-email").val();
     var empresa = $("#contato-empresa").val();
-    var tel = $("#contato-tel").val();
+    var telefone = $("#contato-tel").val();
     var assunto = $("#contato-assunto").val();
     var mensagem = $("#contato-mensagem").val();
  
     $.ajax({
         type: "POST",
         url: "./contato/contato.php",
-        data: "contato-nome=" + nome + "&contato-email=" + email + "&contato-empresa=" + empresa + "&contato-tel=" + tel + "&contato-assunto=" + assunto + "&contato-mensagem=" + mensagem,
+        data: "contato-nome=" + nome + "&contato-email=" + email + "&contato-empresa=" + empresa + "&contato-tel=" + telefone + "&contato-assunto=" + assunto + "&contato-mensagem=" + mensagem,
         success : function(text){
             if (text == "success"){
                 contatoSuccess();

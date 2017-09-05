fd6a446791b4f1a6228ed5a4be664bc8d1477375
Raphael Pais <oproprio@raphaelrpais.com>
2017-09-05 17:35:51 -0300

    tags html

--- novo/contato/contato.php
+++ novo/contato/contato.php
@@ -51,7 +51,7 @@ $Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
 //$Body .= $mensagem;
 //$Body .= "\n";
 
-$Body .= "$nome, enviou um <b>contato</b> pelo site com o seguinte recado: $mensagem";
+$Body .= "<html><body>$nome, enviou um <b>contato</b> pelo site com o seguinte recado: $mensagem</body></html>";
 
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";

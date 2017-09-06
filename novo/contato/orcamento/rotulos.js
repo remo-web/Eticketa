ce769ad35c10dd4848dcaf8e39c6fb9582262a8b
Raphael Pais <oproprio@raphaelrpais.com>
2017-09-06 15:20:33 -0300

    /orcamento

--- novo/contato/orcamento/rotulos.js
+++ novo/contato/orcamento/rotulos.js
@@ -66,7 +66,7 @@ function submitRotulos(){
  
     $.ajax({
         type: "POST",
-        url: "./contato/orcamento/rotulos.php",
+        url: "./contato/orcamento",
         data : form_data,
         contentType: false,
         cache: false,

929d3fbf428eb8a98cd343f66296114dcd737f22
Raphael Pais <oproprio@raphaelrpais.com>
2017-09-06 17:23:37 -0300

    no-json

--- novo/contato/orcamento/rotulos.js
+++ novo/contato/orcamento/rotulos.js
@@ -69,7 +69,7 @@ function submitRotulos(){
         url: "./contato/orcamento/rotulos.php",
         data: form_data,
         contentType: false,
-        dataType: "json",
+        //dataType: "json",
         cache: false,
         processData:false,
         //data: "o_rotulos-nome=" + nome + "&o_rotulos-email=" + email + "&o_rotulos-empresa=" + empresa + "&o_rotulos-telefone=" + telefone + "&o_rotulos-largura=" + largura + "&o_rotulos-altura=" + altura + "&o_rotulos-formato=" + formato + "&o_rotulos-quantidade=" + quantidade + "&o_rotulos-frente=" + frente + "&o_rotulos-verso=" + verso + "&o_rotulos-finalidade=" + finalidade + "&o_rotulos-mensagem=" + mensagem,

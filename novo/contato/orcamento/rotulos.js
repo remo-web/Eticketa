2f32319f736225376ca0ec2cd1bf60401156304e
Raphael Pais <oproprio@raphaelrpais.com>
2017-09-06 14:55:16 -0300

    Oremos!

--- novo/contato/orcamento/rotulos.js
+++ novo/contato/orcamento/rotulos.js
@@ -4,6 +4,11 @@ $("#etka_o-rotulos").submit(function(event){
 //    var anexo = document.forms["etka_o-rotulos"]["o_rotulos-anexo"].value;
 //    var rotulos_anexo = document.getElementById("o_rotulos-anexo");
     var att = document.createAttribute("required");
+    
+    //anexo
+    var allowed_file_size = "1048576";
+    var allowed_files = ['image/png', 'image/gif', 'image/jpeg', 'image/pjpeg'];
+    
     if (email == "") {
         rotulos_email.setAttributeNode(att);
         return false;
@@ -19,7 +24,23 @@ $("#etka_o-rotulos").submit(function(event){
     if (event.isDefaultPrevented()) {
         // handle the invalid form...
         rotulosError();
-    } else {
+    }	
+    if(window.File && window.FileReader && window.FileList && window.Blob){
+        var total_files_size = 0;
+        $(this.elements['file_attach[]'].files).each(function(i, ifile){
+            if(ifile.value !== ""){ //continue only if file(s) are selected
+                if(allowed_files.indexOf(ifile.type) === -1){ //check unsupported file
+                    alert( ifile.name + " is unsupported file type!");
+                    proceed = false;
+                }
+                total_files_size = total_files_size + ifile.size; //add file size to total size
+            }
+        }); 
+        if(total_files_size > allowed_file_size){ 
+            alert( "Make sure total file size is less than 1 MB!");
+            proceed = false;
+        }
+	} else {
         // everything looks good!
         event.preventDefault();
         submitRotulos();
@@ -39,12 +60,18 @@ function submitRotulos(){
     var frente = $("#o_rotulos-frente").val();
     var verso = $("#o_rotulos-verso").val();
     var finalidade = $("#o_rotulos-finalidade").val();
-    var mensagem = $("#o_rotulos-mensagem").val();
+    var mensagem = $("#o_rotulos-mensagem").val(); 
+    
+		var form_data = new FormData(this); //Creates new FormData object
  
     $.ajax({
         type: "POST",
         url: "./contato/orcamento/rotulos.php",
-        data: "o_rotulos-nome=" + nome + "&o_rotulos-email=" + email + "&o_rotulos-empresa=" + empresa + "&o_rotulos-telefone=" + telefone + "&o_rotulos-largura=" + largura + "&o_rotulos-altura=" + altura + "&o_rotulos-formato=" + formato + "&o_rotulos-quantidade=" + quantidade + "&o_rotulos-frente=" + frente + "&o_rotulos-verso=" + verso + "&o_rotulos-finalidade=" + finalidade + "&o_rotulos-mensagem=" + mensagem,
+        data : form_data,
+        contentType: false,
+        cache: false,
+        processData:false,
+        //data: "o_rotulos-nome=" + nome + "&o_rotulos-email=" + email + "&o_rotulos-empresa=" + empresa + "&o_rotulos-telefone=" + telefone + "&o_rotulos-largura=" + largura + "&o_rotulos-altura=" + altura + "&o_rotulos-formato=" + formato + "&o_rotulos-quantidade=" + quantidade + "&o_rotulos-frente=" + frente + "&o_rotulos-verso=" + verso + "&o_rotulos-finalidade=" + finalidade + "&o_rotulos-mensagem=" + mensagem,
         success : function(text){
             if (text == "success"){
                 rotulosSuccess();
--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -20,11 +20,66 @@ $frente = $_POST["o_rotulos-frente"];
 $verso = $_POST["o_rotulos-verso"];
 $finalidade = $_POST["o_rotulos-finalidade"];
 $mensagem = $_POST["o_rotulos-mensagem"];
+
+$attachments = $_FILES['file_attach'];
+$file_count = count($attachments['name']); //count total files attached
+$boundary = md5("sanwebe.com"); 
  
 $To = "raphael.pais@eticketa.com.br";
 $uglySubject = "[Site | Orçamento] Rótulos";
 $Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
- 
+
+    if($file_count > 0){ //if attachment exists
+        //header
+        $headers = "MIME-Version: 1.0\r\n"; 
+        $headers .= "From:".$from_email."\r\n"; 
+        $headers .= "Reply-To: ".$sender_email."" . "\r\n";
+        $headers .= "Content-Type: multipart/mixed; boundary = $boundary\r\n\r\n"; 
+        
+        //message text
+        $body = "--$boundary\r\n";
+        $body .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
+        $body .= "Content-Transfer-Encoding: base64\r\n\r\n"; 
+        $body .= chunk_split(base64_encode($message)); 
+
+        //attachments
+        for ($x = 0; $x < $file_count; $x++){       
+            if(!empty($attachments['name'][$x])){
+                
+                if($attachments['error'][$x]>0) //exit script and output error if we encounter any
+                {
+                    $mymsg = array( 
+                    1=>"The uploaded file exceeds the upload_max_filesize directive in php.ini", 
+                    2=>"The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form", 
+                    3=>"The uploaded file was only partially uploaded", 
+                    4=>"No file was uploaded", 
+                    6=>"Missing a temporary folder" ); 
+                    print  json_encode( array('type'=>'error',$mymsg[$attachments['error'][$x]]) ); 
+					exit;
+                }
+                
+                //get file info
+                $file_name = $attachments['name'][$x];
+                $file_size = $attachments['size'][$x];
+                $file_type = $attachments['type'][$x];
+                
+                //read file 
+                $handle = fopen($attachments['tmp_name'][$x], "r");
+                $content = fread($handle, $file_size);
+                fclose($handle);
+                $encoded_content = chunk_split(base64_encode($content)); //split into smaller chunks (RFC 2045)
+                
+                $body .= "--$boundary\r\n";
+                $body .="Content-Type: $file_type; name=".$file_name."\r\n";
+                $body .="Content-Disposition: attachment; filename=".$file_name."\r\n";
+                $body .="Content-Transfer-Encoding: base64\r\n";
+                $body .="X-Attachment-Id: ".rand(1000,99999)."\r\n\r\n"; 
+                $body .= $encoded_content; 
+            }
+        }
+
+    }else{
+
 // prepare email body text
 $Body .= "Nome: ";
 $Body .= $nome;
@@ -82,7 +137,8 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
 $headers .= "Content-Type: text/plain; charset=UTF-8" . "\r\n";
 $headers .= "From: $email" . "\r\n";
- 
+
+}
 // send email
 $success = mail($To, $Subject, $Body, $headers);
  

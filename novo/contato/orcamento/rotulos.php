a7b8fa310d633824e92b898e5bb532b9be36cccc
agabiabreu <gabriela.abreu88@gmail.com>
2017-09-25 18:32:26 -0300

    arrumando

--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -23,13 +23,9 @@ $uglySubject = "[Site | Orçamento] Rótulos";
 $Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
  
 // prepare email body text
-$Body .= "Nome: ";
-$Body .= $nome;
-$Body .= "\n";
+$Body .= "Nome: $nome \n ";
  
-$Body .= "E-mail: ";
-$Body .= $email;
-$Body .= "\n";
+$Body .= "E-mail: $email \n";
  
 $Body .= "Cargo / Empresa: ";
 $Body .= $empresa;
@@ -75,31 +71,12 @@ $Body .= "Observações: ";
 $Body .= $mensagem;
 $Body .= "\n";
 
-$arquivo = isset($_FILES["file_attach[]"]) ? $_FILES["file_attach[]"] : FALSE;
-if(!empty($email_)){
-$fp = fopen($_FILES["file_attach[]"]["tmp_name"],"rb");
-$anexo = fread($fp,filesize($_FILES["file_attach[]"]["tmp_name"]));
-$anexo = base64_encode($anexo);fclose($fp);
-$anexo = chunk_split($anexo);$boundary = "XYZ-" . date("dmYis") . "-ZYX";
-$mens = "--$boundary\n";
-$mens .= "Content-Transfer-Encoding: 8bits\n";
-$mens .= "Content-Type: text/html; charset=\"ISO-8859-1\"\n\n";
-//plain
-$mens .= "$Body\n";
-$mens .= "--$boundary\n";
-$mens .= "Content-Type: ".$arquivo["type"]."\n";
-$mens .= "Content-Disposition: attachment; filename=\"".
-$arquivo["name"]."\"\n";
-$mens .= "Content-Transfer-Encoding: base64\n\n";
-$mens .= "$anexo\n";
-$mens .= "--$boundary--\r\n";
-
 
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
-$headers .= "Content-Type: multipart/mixed; charset=UTF-8" . "\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8" . "\r\n";
 $headers .= "From: $email" . "\r\n";
-$headers .= "$boundary\n";
+
  
 // send email
 $success = mail($To, $Subject, $mens, $headers);

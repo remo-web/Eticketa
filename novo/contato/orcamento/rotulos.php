97c1fc8e35d31dd235585cfa55ada9d3dd6bf3df
agabiabreu <gabriela.abreu88@gmail.com>
2017-09-25 18:12:26 -0300

    gogogo

--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -74,9 +74,32 @@ $Body .= "\n";
 $Body .= "Observações: ";
 $Body .= $mensagem;
 $Body .= "\n";
+
+$arquivo = isset($_FILES["o_rotulos-anexo"]) ? $_FILES["o_rotulos-anexo"] : FALSE;
+
+if(file_exists($arquivo["tmp_name"]) and !empty($arquivo)){
+   $fp = fopen($_FILES["o_rotulos-anexo"]["tmp_name"],"rb");        
+$anexo = fread($fp,filesize($_FILES["o_rotulos-anexo"]["tmp_name"]));                  
+$anexo = base64_encode($anexo);fclose($fp);       
+$anexo = chunk_split($anexo);$boundary = "XYZ-" . date("dmYis") . "-ZYX";        
+$mens = "--$boundary\n";        
+$mens .= "Content-Transfer-Encoding: 8bits\n";        
+$mens .= "Content-Type: text/html; charset=\"ISO-8859-1\"\n\n"; 
+//plain        
+$mens .= "$Body\n";        
+$mens .= "--$boundary\n";        
+$mens .= "Content-Type: ".$arquivo["type"]."\n";        
+$mens .= "Content-Disposition: attachment; filename=\"".
+$arquivo["name"]."\"\n";        
+$mens .= "Content-Transfer-Encoding: base64\n\n";       
+$mens .= "$anexo\n";       
+$mens .= "--$boundary--\r\n";
+
+
+
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
-$headers .= "Content-Type: text/plain; charset=UTF-8" . "\r\n";
+$headers .= "Content-Type: multipart/mixed; charset=UTF-8" . "\r\n";
 $headers .= "From: $email" . "\r\n";
  
 // send email

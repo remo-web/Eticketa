f9b9509699ef1e4d5f0aa609cd8a1cfdb45c58ea
agabiabreu <gabriela.abreu88@gmail.com>
2017-09-25 16:49:22 -0300

    volta com body

--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -83,18 +83,18 @@ $anexo = base64_encode($anexo);
 fclose($fp); 
 $anexo = chunk_split($anexo); 
     
-$mens = "--$boundary\n";        
-$mens .= "Content-Transfer-Encoding: 8bits\n";        
-$mens .= "Content-Type: text/html; charset=\"ISO-8859-1\"\n\n"; 
+$Body = "--$boundary\n";        
+$Body .= "Content-Transfer-Encoding: 8bits\n";        
+$Body .= "Content-Type: text/html; charset=\"ISO-8859-1\"\n\n"; 
 //plain        
-$mens .= "$mensagem\n";        
-$mens .= "--$boundary\n";        
-$mens .= "Content-Type: ".$arquivo["type"]."\n";        
-$mens .= "Content-Disposition: attachment; filename=\"".
+$Body .= "$mensagem\n";        
+$Body .= "--$boundary\n";        
+$Body .= "Content-Type: ".$arquivo["type"]."\n";        
+$Body .= "Content-Disposition: attachment; filename=\"".
 $arquivo["name"]."\"\n";        
-$mens .= "Content-Transfer-Encoding: base64\n\n";       
-$mens .= "$anexo\n";       
-$mens .= "--$boundary--\r\n";
+$Body .= "Content-Transfer-Encoding: base64\n\n";       
+$Body .= "$anexo\n";       
+$Body .= "--$boundary--\r\n";
     
 
 $headers = "MIME-Version: 1.0" . "\r\n";
@@ -104,7 +104,7 @@ $headers .= "From: $email" . "\r\n";
 $headers .= "$boundary\n";
  
 // send email
-mail($To, $Subject, $mens, $headers);
+mail($To, $Subject, $Body, $headers);
  
 // redirect to success page
 /*if ($success && $error == ""){

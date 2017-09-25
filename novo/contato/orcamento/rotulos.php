e3c4126224c0ac6e64748dc329f183cf9e1a8754
agabiabreu <gabriela.abreu88@gmail.com>
2017-09-25 17:50:30 -0300

    tentando qualquer coisa

--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -175,32 +175,13 @@ $mensagem .= "Observações: ";
 $mensagem .= $texto;
 $mensagem .= "\n";
 
-$arquivo = isset($_FILES["file_attach[]"]) ? $_FILES["file_attach[]"] : FALSE;
-
-if(file_exists($arquivo["tmp_name"]) and !empty($arquivo)){
-   $fp = fopen($_FILES["file_attach[]"]["tmp_name"],"rb");        
-$anexo = fread($fp,filesize($_FILES["file_attach[]"]["tmp_name"]));                  
-$anexo = base64_encode($anexo);fclose($fp);       
-$anexo = chunk_split($anexo);$boundary = "XYZ-" . date("dmYis") . "-ZYX";        
-$mens = "--$boundary\n";        
-$mens .= "Content-Transfer-Encoding: 8bits\n";        
-$mens .= "Content-Type: text/html; charset=\"ISO-8859-1\"\n\n"; 
-//plain        
-$mens .= "$mensagem\n";        
-$mens .= "--$boundary\n";        
-$mens .= "Content-Type: ".$arquivo["type"]."\n";        
-$mens .= "Content-Disposition: attachment; filename=\"".
-$arquivo["name"]."\"\n";        
-$mens .= "Content-Transfer-Encoding: base64\n\n";       
-$mens .= "$anexo\n";       
-$mens .= "--$boundary--\r\n";
 $headers  = "MIME-Version: 1.0\n";
 $headers .= "From: \"$nome\" <$email_>\r\n";
 $headers .= "Content-type: multipart/mixed; boundary=\"$boundary\"\r\n";
 $headers .= "$boundary\n";
     
 // send email
-$success = mail($To, $Subject, $mens, $headers);
+$success = mail($To, $Subject, $mensagem, $headers);
  
 // redirect to success page
 if ($success && $error == ""){

343bfe83357b59e8b9574553aaaacad8c40d0ed9
agabiabreu <gabriela.abreu88@gmail.com>
2017-09-25 16:40:50 -0300

    agora vai!

--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -18,14 +18,6 @@ $verso = $_POST["o_rotulos-verso"];
 $finalidade = $_POST["o_rotulos-finalidade"];
 $mensagem = $_POST["o_rotulos-mensagem"];
 
-$arquivo = isset($_FILES["o-rotulos-anexo"]) ? $_FILES["o-rotulos-anexo"] : FALSE; 
-if(file_exists($arquivo["tmp_name"]) and !empty($arquivo)){ 
-$fp = fopen($_FILES["arquivo"]["tmp_name"],"rb"); 
-$anexo = fread($fp,filesize($_FILES["o-rotulos-anexo"]["tmp_name"])); 
-$anexo = base64_encode($anexo); 
-fclose($fp); 
-$anexo = chunk_split($anexo);     
- 
 $To = "raphael.pais@eticketa.com.br";
 $uglySubject = "[Site | Orçamento] Rótulos";
 $Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
@@ -82,15 +74,37 @@ $Body .= "\n";
 $Body .= "Observações: ";
 $Body .= $mensagem;
 $Body .= "\n";
+
+$arquivo = isset($_FILES["o-rotulos-anexo"]) ? $_FILES["o-rotulos-anexo"] : FALSE; 
+if(file_exists($arquivo["tmp_name"]) and !empty($arquivo)){ 
+$fp = fopen($_FILES["o-rotulos-anexo"]["tmp_name"],"rb"); 
+$anexo = fread($fp,filesize($_FILES["o-rotulos-anexo"]["tmp_name"])); 
+$anexo = base64_encode($anexo); 
+fclose($fp); 
+$anexo = chunk_split($anexo); 
+    
+$mens = "--$boundary\n";        
+$mens .= "Content-Transfer-Encoding: 8bits\n";        
+$mens .= "Content-Type: text/html; charset=\"ISO-8859-1\"\n\n"; 
+//plain        
+$mens .= "$mensagem\n";        
+$mens .= "--$boundary\n";        
+$mens .= "Content-Type: ".$arquivo["type"]."\n";        
+$mens .= "Content-Disposition: attachment; filename=\"".
+$arquivo["name"]."\"\n";        
+$mens .= "Content-Transfer-Encoding: base64\n\n";       
+$mens .= "$anexo\n";       
+$mens .= "--$boundary--\r\n";
     
 
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
 $headers .= "Content-Type: multipart/mixed; charset=UTF-8" . "\r\n";
 $headers .= "From: $email" . "\r\n";
+$headers .= "$boundary\n";
  
 // send email
-$success = mail($To, $Subject, $Body, $headers);
+$success = mail($To, $Subject, $Body, $mens, $headers);
  
 // redirect to success page
 if ($success && $error == ""){

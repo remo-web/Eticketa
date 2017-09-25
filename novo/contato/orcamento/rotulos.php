544f73a88b1df89dc3d713ebd7a619b291664568
Raphael <raphaelrpais@gmail.com>
2017-09-25 15:22:06 -0300

    coming back again

--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -17,11 +17,8 @@ $frente = $_POST["o_rotulos-frente"];
 $verso = $_POST["o_rotulos-verso"];
 $finalidade = $_POST["o_rotulos-finalidade"];
 $mensagem = $_POST["o_rotulos-mensagem"];
-
-$attachments = $_FILES['file_attach'];
-$file_count = count($attachments['name']);
-$boundary = md5("eticketa.com.br");
-
+ 
+$To = "raphael.pais@eticketa.com.br";
 $uglySubject = "[Site | Orçamento] Rótulos";
 $Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
  
@@ -77,49 +74,10 @@ $Body .= "\n";
 $Body .= "Observações: ";
 $Body .= $mensagem;
 $Body .= "\n";
-
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
-$headers .= "Content-Type: multipart/mixed" . "\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8" . "\r\n";
 $headers .= "From: $email" . "\r\n";
-
-        //attachments
-        for ($x = 0; $x < $file_count; $x++){       
-            if(!empty($attachments['name'][$x])){
-                
-                if($attachments['error'][$x]>0) //exit script and output error if we encounter any
-                {
-                    $mymsg = array( 
-                    1=>"The uploaded file exceeds the upload_max_filesize directive in php.ini", 
-                    2=>"The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form", 
-                    3=>"The uploaded file was only partially uploaded", 
-                    4=>"No file was uploaded", 
-                    6=>"Missing a temporary folder" ); 
-                    print  json_encode( array('type'=>'error',$mymsg[$attachments['error'][$x]]) ); 
-                    exit;
-                }
-                
-                //get file info
-                $file_name = $attachments['name'][$x];
-                $file_size = $attachments['size'][$x];
-                $file_type = $attachments['type'][$x];
-                
-                //read file 
-                $handle = fopen($attachments['tmp_name'][$x], "r");
-                $content = fread($handle, $file_size);
-                fclose($handle);
-                $encoded_content = chunk_split(base64_encode($content)); //split into smaller chunks (RFC 2045)
-                
-                $body .= "--$boundary\r\n";
-                $body .="Content-Type: $file_type; name=".$file_name."\r\n";
-                $body .="Content-Disposition: attachment; filename=".$file_name."\r\n";
-                $body .="Content-Transfer-Encoding: base64\r\n";
-                $body .="X-Attachment-Id: ".rand(1000,99999)."\r\n\r\n"; 
-                $body .= $encoded_content; 
-            }
-        }
- 
-$To = "raphael.pais@eticketa.com.br";
  
 // send email
 $success = mail($To, $Subject, $Body, $headers);

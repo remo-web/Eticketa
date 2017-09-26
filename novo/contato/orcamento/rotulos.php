8f1507b1a4ee7110a38384d1cf7aabd0ec3078f2
agabiabreu <gabriela.abreu88@gmail.com>
2017-09-26 14:26:06 -0300

    back to reality

--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -17,13 +17,12 @@ $frente = $_POST["o_rotulos-frente"];
 $verso = $_POST["o_rotulos-verso"];
 $finalidade = $_POST["o_rotulos-finalidade"];
 $mensagem = $_POST["o_rotulos-mensagem"];
-$anexo = $_FILES[file_attach];
 
 $To = "raphael.pais@eticketa.com.br";
 $uglySubject = "[Site | Orçamento] Rótulos";
 $Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
  
-/* prepare email body text
+// prepare email body text
 $Body .= "Nome: ";
 $Body .= $nome;
 $Body .= "\n";
@@ -74,68 +73,15 @@ $Body .= "\n";
  
 $Body .= "Observações: ";
 $Body .= $mensagem;
-$Body .= "\n"; */
+$Body .= "\n";
 
-$file_count = count($anexo['name']); //count total files attached
-    $boundary = md5("sanwebe.com"); 
-
-if($file_count > 0){ //if attachment exists
-        //header
-$headers = "MIME-Version: 1.0\r\n";
-$headers .= "From: $email" . "\r\n";
+$headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
-$headers .= "Content-Type: multipart/mixed; boundary = $boundary charset=UTF-8\r\n\r\n";
-
-        //message text
-$body = "--$boundary\r\n";
-$body .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
-$body .= "Content-Transfer-Encoding: base64\r\n\r\n"; 
-$body .= chunk_split(base64_encode($mensagem)); 
-
-for ($x = 0; $x < $file_count; $x++){       
-            if(!empty($anexo['name'][$x])){
-                
-                if($anexo['error'][$x]>0) //exit script and output error if we encounter any
-                {
-                    $mymsg = array( 
-                    1=>"The uploaded file exceeds the upload_max_filesize directive in php.ini", 
-                    2=>"The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form", 
-                    3=>"The uploaded file was only partially uploaded", 
-                    4=>"No file was uploaded", 
-                    6=>"Missing a temporary folder" ); 
-                    die($mymsg[$anexo['error'][$x]]); 
-                }
-                
-                //get file info
-                $file_name = $anexo['name'][$x];
-                $file_size = $anexo['size'][$x];
-                $file_type = $anexo['type'][$x];
-                
-                //read file 
-                $handle = fopen($anexo['tmp_name'][$x], "r");
-                $content = fread($handle, $file_size);
-                fclose($handle);
-                $encoded_content = chunk_split(base64_encode($content)); //split into smaller chunks (RFC 2045)
-                
-                $body .= "--$boundary\r\n";
-                $body .="Content-Type: $file_type; name="$file_name"\r\n";
-                $body .="Content-Disposition: attachment; filename="$file_name"\r\n";
-                $body .="Content-Transfer-Encoding: base64\r\n";
-                $body .="X-Attachment-Id: ".rand(1000,99999)."\r\n\r\n"; 
-                $body .= $encoded_content; 
-            }
-        }
-
-    }else{ //send plain email otherwise
-       $headers = "From:".$To."\r\n".
-        "Reply-To: ".$email. "\n" .
-        "X-Mailer: PHP/" . phpversion();
-        $body = $mensagem;
-    }
-
+$headers .= "Content-Type: text/plain; charset=UTF-8" . "\r\n";
+$headers .= "From: $email" . "\r\n";
  
 // send email
-$success = mail($To, $Subject, $body, $headers);
+$success = mail($To, $Subject, $Body, $headers);
  
 // redirect to success page
 if ($success && $error == ""){

44079aca517f3444f333400419f2b759740abfde
agabiabreu <gabriela.abreu88@gmail.com>
2017-09-26 13:54:58 -0300

    try again

--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -1,55 +1,101 @@
 <?php
-if($_POST && isset($_FILES['file_attach']))
-{
-    $recipient_email    = "gabriela.abreu88@gmail.com"; //recepient
-    $from_email         = "raphael.pais@eticketa.com.br"; //from email using site domain.
-    $subject            = "Trocar isso depois: Attachment email from your website!"; //email subject line
-    
-    $sender_name = filter_var($_POST["o_rotulos-nome"], FILTER_SANITIZE_STRING); //capture sender name
-    $sender_email = filter_var($_POST["o_rotulos-email"], FILTER_SANITIZE_STRING); //capture sender email
-    $telefone = filter_var ($_POST["o_rotulos-telefone"], FILTER_SANITIZE_STRING);
-    $largura = filter_var ($_POST["o_rotulos-largura"], FILTER_SANITIZE_STRING);
-    $altura = filter_var ($_POST["o_rotulos-altura"], FILTER_SANITIZE_STRING);
-    $formato = filter_var ($_POST["o_rotulos-formato"], FILTER_SANITIZE_STRING);
-    $quantidade = filter_var ($_POST["o_rotulos-quantidade"], FILTER_SANITIZE_STRING);
-    $frente = filter_var ($_POST["o_rotulos-frente"], FILTER_SANITIZE_STRING);
-    $verso = filter_var ($_POST["o_rotulos-verso"], FILTER_SANITIZE_STRING);
-    $finalidade = filter_var ($_POST["o_rotulos-finalidade"], FILTER_SANITIZE_STRING);
-    $sender_message = filter_var($_POST["o_rotulos-mensagem"], FILTER_SANITIZE_STRING); //capture message
-    $attachments = $_FILES['file_attach'];
-    
-    //php validation
-    if(strlen($sender_name)<2){
-        die('Nome muito pequeno ou inexistente');
-    }
-    if (!filter_var($sender_email, FILTER_VALIDATE_EMAIL)) {
-      die('Invalid email');
-    }
-    if(strlen($sender_message)<4){
-        die('Mensagem muito curta, por favor escreva algo!');
-    }
-    
-    $file_count = count($attachments['name']); //count total files attached
-    $boundary = md5("sanwebe.com"); 
-            
-    if($file_count > 0){ //if attachment exists
-        //header
-        $headers = "MIME-Version: 1.0\r\n"; 
-        $headers .= "From:".$from_email."\r\n"; 
-        $headers .= "Reply-To: ".$sender_email."" . "\r\n";
-        $headers .= "Content-Type: multipart/mixed; boundary = $boundary\r\n\r\n"; 
-        
-        //message text
-        $body = "--$boundary\r\n";
-        $body .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
-        $body .= "Content-Transfer-Encoding: base64\r\n\r\n"; 
-        $body .= chunk_split(base64_encode($sender_message)); 
+$error = "";
+$nome = $_POST["o_rotulos-nome"];
+//email
+if (empty($_POST["o_rotulos-email"])) {
+    $error .= "Email is required ";
+} else {
+    $email = $_POST["o_rotulos-email"];
+}
+$empresa = $_POST["o_rotulos-empresa"];
+$telefone = $_POST["o_rotulos-telefone"];
+$largura = $_POST["o_rotulos-largura"];
+$altura = $_POST["o_rotulos-altura"];
+$formato = $_POST["o_rotulos-formato"];
+$quantidade = $_POST["o_rotulos-quantidade"];
+$frente = $_POST["o_rotulos-frente"];
+$verso = $_POST["o_rotulos-verso"];
+$finalidade = $_POST["o_rotulos-finalidade"];
+$mensagem = $_POST["o_rotulos-mensagem"];
+$anexo = $_FILES[file_attach];
 
-        //attachments
-        for ($x = 0; $x < $file_count; $x++){       
-            if(!empty($attachments['name'][$x])){
+$To = "raphael.pais@eticketa.com.br";
+$uglySubject = "[Site | Orçamento] Rótulos";
+$Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
+ 
+/* prepare email body text
+$Body .= "Nome: ";
+$Body .= $nome;
+$Body .= "\n";
+ 
+$Body .= "E-mail: ";
+$Body .= $email;
+$Body .= "\n";
+ 
+$Body .= "Cargo / Empresa: ";
+$Body .= $empresa;
+$Body .= "\n";
+ 
+$Body .= "Telefone: ";
+$Body .= $telefone;
+$Body .= "\n";
+ 
+$Body .= "Largura: ";
+$Body .= $largura;
+$Body .= " cm";
+$Body .= "\n";
+ 
+$Body .= "Altura: ";
+$Body .= $altura;
+$Body .= " cm";
+$Body .= "\n";
+ 
+$Body .= "Formato: ";
+$Body .= $formato;
+$Body .= "\n";
+ 
+$Body .= "Quantidade: ";
+$Body .= $quantidade;
+$Body .= "\n";
+ 
+$Body .= "Frente: ";
+$Body .= $frente;
+$Body .= " cores";
+$Body .= "\n";
+ 
+$Body .= "Verso: ";
+$Body .= $verso;
+$Body .= " cores";
+$Body .= "\n";
+ 
+$Body .= "Finalidade: ";
+$Body .= $finalidade;
+$Body .= "\n";
+ 
+$Body .= "Observações: ";
+$Body .= $mensagem;
+$Body .= "\n"; */
+
+$file_count = count($anexo['name']); //count total files attached
+    $boundary = md5("sanwebe.com"); 
+
+if($file_count > 0){ //if attachment exists
+        //header
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "From: $email" . "\r\n";
+$headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
+$headers .= "Content-Type: multipart/mixed; boundary = $boundary charset=UTF-8\r\n\r\n";
+
+        //message text
+$body = "--$boundary\r\n";
+$body .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
+$body .= "Content-Transfer-Encoding: base64\r\n\r\n"; 
+$body .= chunk_split(base64_encode($mensagem)); 
+
+for ($x = 0; $x < $file_count; $x++){       
+            if(!empty($anexo['name'][$x])){
                 
-                if($attachments['error'][$x]>0) //exit script and output error if we encounter any
+                if($anexo['error'][$x]>0) //exit script and output error if we encounter any
                 {
                     $mymsg = array( 
                     1=>"The uploaded file exceeds the upload_max_filesize directive in php.ini", 
@@ -57,16 +103,16 @@ if($_POST && isset($_FILES['file_attach']))
                     3=>"The uploaded file was only partially uploaded", 
                     4=>"No file was uploaded", 
                     6=>"Missing a temporary folder" ); 
-                    die($mymsg[$attachments['error'][$x]]); 
+                    die($mymsg[$anexo['error'][$x]]); 
                 }
                 
                 //get file info
-                $file_name = $attachments['name'][$x];
-                $file_size = $attachments['size'][$x];
-                $file_type = $attachments['type'][$x];
+                $file_name = $anexo['name'][$x];
+                $file_size = $anexo['size'][$x];
+                $file_type = $anexo['type'][$x];
                 
                 //read file 
-                $handle = fopen($attachments['tmp_name'][$x], "r");
+                $handle = fopen($anexo['tmp_name'][$x], "r");
                 $content = fread($handle, $file_size);
                 fclose($handle);
                 $encoded_content = chunk_split(base64_encode($content)); //split into smaller chunks (RFC 2045)
@@ -81,18 +127,24 @@ if($_POST && isset($_FILES['file_attach']))
         }
 
     }else{ //send plain email otherwise
-       $headers = "From:".$from_email."\r\n".
-        "Reply-To: ".$sender_email. "\n" .
+       $headers = "From:".$To."\r\n".
+        "Reply-To: ".$email. "\n" .
         "X-Mailer: PHP/" . phpversion();
-        $body = $sender_message;
+        $body = $mensagem;
     }
-        
-     $sentMail = @mail($recipient_email, $subject, $body, $headers);
-    if($sentMail) //output success or failure messages
-    {       
-        die('Thank you for your email');
-    }else{
-        die('Could not send mail! Please check your PHP mail configuration.');  
+
+ 
+// send email
+$success = mail($To, $Subject, $body, $headers);
+ 
+// redirect to success page
+if ($success && $error == ""){
+    echo "success";
+} else {
+    if($error == ""){
+        echo "Algo deu errado... Mas deu errado num nível, que é melhor você nos ligar no telefone (21) 3490-9292, porque pelo site vai ser difícil.";
+    } else {
+        echo $error;
     }
-}
+} 
 ?>

8d25ac8bbb32fda983b5312dda8d94f80815f9fe
agabiabreu <gabriela.abreu88@gmail.com>
2017-09-26 15:05:00 -0300

    back all

--- novo/contato/orcamento/rotulos.js
+++ novo/contato/orcamento/rotulos.js
@@ -1,67 +1,65 @@
-var allowed_file_size = "1048576"; //allowed file size
-var allowed_files = ['image/png', 'image/gif', 'image/jpeg', 'application/pdf']; //allowed file types
-var border_color = "#C2C2C2"; //initial input border color
-
-$("#etka_o-rotulos").submit(function(e){
-    e.preventDefault(); //prevent default action 
-    proceed = true; //set proceed flat to true
-    
-    //simple input validation
-    $($(this).find("input[data-required=true], textarea[data-required=true]")).each(function(){
-            if(!$.trim($(this).val())){ //if this field is empty 
-                $(this).css('border-color','red'); //change border color to red   
-                proceed = false; //set do not proceed flag
-            }
-            //check invalid email
-            var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/; 
-            if($(this).attr("type")=="email" && !email_reg.test($.trim($(this).val()))){
-                $(this).css('border-color','red'); //change border color to red   
-                proceed = false; //set do not proceed flag              
-            }   
-    }).on("input", function(){ //change border color to original
-         $(this).css('border-color', border_color);
-    });
-    
-    //check file size and type before upload, works in modern browsers
-    if(window.File && window.FileReader && window.FileList && window.Blob){
-        var total_files_size = 0;
-        $(this.elements['file_attach[]'].files).each(function(i, ifile){
-            if(ifile.value !== ""){ //continue only if file(s) are selected
-                if(allowed_files.indexOf(ifile.type) === -1){ //check unsupported file
-                    alert( ifile.name + " is unsupported file type!");
-                    proceed = false;
-                }
-             total_files_size = total_files_size + ifile.size; //add file size to total size
-            }
-        }); 
-       if(total_files_size > allowed_file_size){ 
-            alert( "Make sure total file size is less than 1 MB!");
-            proceed = false;
-        }
+$("#etka_o-rotulos").submit(function(event){
+    var email = document.forms["etka_o-rotulos"]["o_rotulos-email"].value;
+    var rotulos_email = document.getElementById("o_rotulos-email");
+//    var anexo = document.forms["etka_o-rotulos"]["o_rotulos-anexo"].value;
+//    var rotulos_anexo = document.getElementById("o_rotulos-anexo");
+    var att = document.createAttribute("required");
+    if (email == "") {
+        rotulos_email.setAttributeNode(att);
+        return false;
+        // handle the invalid form...
+        rotulosError();
     }
-    
-    //if everything's ok, continue with Ajax form submit
-    if(proceed){ 
-        var post_url = $(this).attr("action"); //get form action url
-        var request_method = $(this).attr("method"); //get form GET/POST method
-        var form_data = new FormData(this); //constructs key/value pairs representing fields and values
-        
-        $.ajax({ //ajax form submit
-            url : post_url,
-            type: request_method,
-            data : form_data,
-            dataType : "json",
-            contentType: false,
-            cache: false,
-            processData:false
-        }).done(function(res){ //fetch server "json" messages when done
-            if(res.type == "error"){
-                $("#dialog-orcamento-rotulos").html('<div class="error">'+ res.text +"</div>");
-            }
-            
-            if(res.type == "done"){
-                $("#dialog-orcamento-rotulos").html('<div class="success">'+ res.text +"</div>");
-            }
-        });
+//    if (anexo == "") {
+//        rotulos_anexo.setAttributeNode(att);
+//        return false;
+//        // handle the invalid form...
+//        rotulosError();
+//    } 
+    if (event.isDefaultPrevented()) {
+        // handle the invalid form...
+        rotulosError();
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        submitRotulos();
     }
 });
+
+function submitRotulos(){
+    // Initiate Variables With Form Content
+    var nome = $("#o_rotulos-nome").val();
+    var email = $("#o_rotulos-email").val();
+    var empresa = $("#o_rotulos-empresa").val();
+    var telefone = $("#o_rotulos-telefone").val();
+    var largura = $("#o_rotulos-largura").val();
+    var altura = $("#o_rotulos-altura").val();
+    var formato = $("input[name=o_rotulos-formato]:checked").val();
+    var quantidade = $("#o_rotulos-quantidade").val();
+    var frente = $("#o_rotulos-frente").val();
+    var verso = $("#o_rotulos-verso").val();
+    var finalidade = $("#o_rotulos-finalidade").val();
+    var mensagem = $("#o_rotulos-mensagem").val();
+ 
+    $.ajax({
+        type: "POST",
+        url: "./contato/orcamento/rotulos.php",
+        data: "o_rotulos-nome=" + nome + "&o_rotulos-email=" + email + "&o_rotulos-empresa=" + empresa + "&o_rotulos-telefone=" + telefone + "&o_rotulos-largura=" + largura + "&o_rotulos-altura=" + altura + "&o_rotulos-formato=" + formato + "&o_rotulos-quantidade=" + quantidade + "&o_rotulos-frente=" + frente + "&o_rotulos-verso=" + verso + "&o_rotulos-finalidade=" + finalidade + "&o_rotulos-mensagem=" + mensagem,
+        success : function(text){
+            if (text == "success"){
+                rotulosSuccess();
+            } else {
+                rotulosError();
+            }
+        }
+    });
+}
+
+function rotulosSuccess(){
+    $( "#etka_o-rotulos-enviado" ).removeClass( "etka_o-rotulos-enviado" );
+    $( '#o_rotulos-nome, #o_rotulos-email, #o_rotulos-empresa, #o_rotulos-telefone, #o_rotulos-largura, #o_rotulos-altura, #o_rotulos-formato, #o_rotulos-quantidade, #o_rotulos-frente, #o_rotulos-verso, #o_rotulos-finalidade, #o_rotulos-mensagem' ).val('');
+}
+
+function rotulosError(){
+    $( "#etka_o-rotulos-erro" ).removeClass( "etka_o-rotulos-erro" );
+}
--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -1,121 +1,96 @@
 <?php
-$recipient_email    = "raphael.pais@eticketa.com.br"; //recepient
-$from_email         = "raphael.pais@eticketa.com.br"; //from email using site domain.
-
-
-if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
-    die('Sorry Request must be Ajax POST'); //exit script
+$error = "";
+$nome = $_POST["o_rotulos-nome"];
+//email
+if (empty($_POST["o_rotulos-email"])) {
+    $error .= "Email is required ";
+} else {
+    $email = $_POST["o_rotulos-email"];
 }
+$empresa = $_POST["o_rotulos-empresa"];
+$telefone = $_POST["o_rotulos-telefone"];
+$largura = $_POST["o_rotulos-largura"];
+$altura = $_POST["o_rotulos-altura"];
+$formato = $_POST["o_rotulos-formato"];
+$quantidade = $_POST["o_rotulos-quantidade"];
+$frente = $_POST["o_rotulos-frente"];
+$verso = $_POST["o_rotulos-verso"];
+$finalidade = $_POST["o_rotulos-finalidade"];
+$mensagem = $_POST["o_rotulos-mensagem"];
 
-if($_POST){
-    
-    $sender_name    = filter_var($_POST["o_rotulos-nome"], FILTER_SANITIZE_STRING); //capture sender name
-    $sender_email   = filter_var($_POST["o_rotulos-email"], FILTER_SANITIZE_STRING); //capture sender email
-    $sender_empresa   = filter_var($_POST["o_rotulos-empresa"], FILTER_SANITIZE_STRING); //capture sender empresa
-    $sender_email   = filter_var($_POST["o_rotulos-email"], FILTER_SANITIZE_STRING); //capture sender email
-    $phone_number   = filter_var($_POST["o_rotulos-telefone"], FILTER_SANITIZE_NUMBER_INT);
-    $largura   = filter_var($_POST["o_rotulos-largura"], FILTER_SANITIZE_NUMBER_INT);
-    $altura   = filter_var($_POST["o_rotulos-altura"], FILTER_SANITIZE_NUMBER_INT);
-    $formato   = filter_var($_POST["o_rotulos-formato"], FILTER_SANITIZE_STRING);
-    $quantidade   = filter_var($_POST["o_rotulos-quantidade"], FILTER_SANITIZE_NUMBER_INT);
-    $frente   = filter_var($_POST["o_rotulos-frente"], FILTER_SANITIZE_NUMBER_INT);
-    $verso   = filter_var($_POST["o_rotulos-verso"], FILTER_SANITIZE_NUMBER_INT);
-    $finalidade   = filter_var($_POST["o_rotulos-finalidade"], FILTER_SANITIZE_STRING);
-    $subject        = "[Site | Orçamento] Rótulos";
-    $message        = filter_var($_POST["o_rotulos-mensagem"], FILTER_SANITIZE_STRING); //capture message
+$To = "raphael.pais@eticketa.com.br";
+$uglySubject = "[Site | Orçamento] Rótulos";
+$Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
+ 
+// prepare email body text
+$Body .= "Nome: ";
+$Body .= $nome;
+$Body .= "\n";
+ 
+$Body .= "E-mail: ";
+$Body .= $email;
+$Body .= "\n";
+ 
+$Body .= "Cargo / Empresa: ";
+$Body .= $empresa;
+$Body .= "\n";
+ 
+$Body .= "Telefone: ";
+$Body .= $telefone;
+$Body .= "\n";
+ 
+$Body .= "Largura: ";
+$Body .= $largura;
+$Body .= " cm";
+$Body .= "\n";
+ 
+$Body .= "Altura: ";
+$Body .= $altura;
+$Body .= " cm";
+$Body .= "\n";
+ 
+$Body .= "Formato: ";
+$Body .= $formato;
+$Body .= "\n";
+ 
+$Body .= "Quantidade: ";
+$Body .= $quantidade;
+$Body .= "\n";
+ 
+$Body .= "Frente: ";
+$Body .= $frente;
+$Body .= " cores";
+$Body .= "\n";
+ 
+$Body .= "Verso: ";
+$Body .= $verso;
+$Body .= " cores";
+$Body .= "\n";
+ 
+$Body .= "Finalidade: ";
+$Body .= $finalidade;
+$Body .= "\n";
+ 
+$Body .= "Observações: ";
+$Body .= $mensagem;
+$Body .= "\n";
 
-    $attachments = $_FILES['file_attach'];
-    
-    
-    //php validation
-    if(strlen($sender_name)<4){ // If length is less than 4 it will output JSON error.
-        print json_encode(array('type'=>'error', 'text' => 'Name is too short or empty!'));
-        exit;
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8" . "\r\n";
+$headers .= "From: $email" . "\r\n";
+ 
+// send email
+$success = mail($To, $Subject, $Body, $headers);
+ 
+// redirect to success page
+if ($success && $error == ""){
+    echo "success";
+} else {
+    if($error == ""){
+        echo "Algo deu errado... Mas deu errado num nível, que é melhor você nos ligar no telefone (21) 3490-9292, porque pelo site vai ser difícil.";
+    } else {
+        echo $error;
     }
-    if(!filter_var($sender_email, FILTER_VALIDATE_EMAIL)){ //email validation
-        print json_encode(array('type'=>'error', 'text' => 'Please enter a valid email!'));
-        exit;
-    }
-    if(!filter_var($largura, FILTER_VALIDATE_INT)){ //check for valid numbers in country code field
-        $output = json_encode(array('type'=>'error', 'text' => 'Enter only digits in country code'));
-        exit;
-    }
-    if(!filter_var($phone_number, FILTER_SANITIZE_NUMBER_FLOAT)){ //check for valid numbers in phone number field
-        print json_encode(array('type'=>'error', 'text' => 'Enter only digits in phone number'));
-        exit;
-    }
-    if(strlen($message)<3){ //check emtpy message
-        print json_encode(array('type'=>'error', 'text' => 'Too short message! Please enter something.'));
-        exit;
-    }
-
-    
-    $file_count = count($attachments['name']); //count total files attached
-    $boundary = md5("sanwebe.com"); 
-            
-    if($file_count > 0){ //if attachment exists
-        //header
-        $headers = "MIME-Version: 1.0\r\n"; 
-        $headers .= "From:".$from_email."\r\n"; 
-        $headers .= "Reply-To: ".$sender_email."" . "\r\n";
-        $headers .= "Content-Type: multipart/mixed; boundary = $boundary\r\n\r\n"; 
-        
-        //message text
-        $body = "--$boundary\r\n";
-        $body .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
-        $body .= "Content-Transfer-Encoding: base64\r\n\r\n"; 
-        $body .= chunk_split(base64_encode($message)); 
-
-        //attachments
-        for ($x = 0; $x < $file_count; $x++){       
-            if(!empty($attachments['name'][$x])){
-                
-                if($attachments['error'][$x]>0) //exit script and output error if we encounter any
-                {
-                    $mymsg = array( 
-                    1=>"The uploaded file exceeds the upload_max_filesize directive in php.ini", 
-                    2=>"The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form", 
-                    3=>"The uploaded file was only partially uploaded", 
-                    4=>"No file was uploaded", 
-                    6=>"Missing a temporary folder" ); 
-                    print  json_encode( array('type'=>'error',$mymsg[$attachments['error'][$x]]) ); 
-                    exit;
-                }
-                
-                //get file info
-                $file_name = $attachments['name'][$x];
-                $file_size = $attachments['size'][$x];
-                $file_type = $attachments['type'][$x];
-                
-                //read file 
-                $handle = fopen($attachments['tmp_name'][$x], "r");
-                $content = fread($handle, $file_size);
-                fclose($handle);
-                $encoded_content = chunk_split(base64_encode($content)); //split into smaller chunks (RFC 2045)
-                
-                $body .= "--$boundary\r\n";
-                $body .="Content-Type: $file_type; name=".$file_name."\r\n";
-                $body .="Content-Disposition: attachment; filename=".$file_name."\r\n";
-                $body .="Content-Transfer-Encoding: base64\r\n";
-                $body .="X-Attachment-Id: ".rand(1000,99999)."\r\n\r\n"; 
-                $body .= $encoded_content; 
-            }
-        }
-
-    }else{ //send plain email otherwise
-       $headers = "From:".$from_email."\r\n".
-        "Reply-To: ".$sender_email. "\n" .
-        "X-Mailer: PHP/" . phpversion();
-        $body = $message;
-    }
-        
-    $sentMail = mail($recipient_email, $subject, $body, $headers);
-    if($sentMail) //output success or failure messages
-    {       
-        print json_encode(array('type'=>'done', 'text' => 'Thank you for your email'));
-        exit;
-    }else{
-        print json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));  
-        exit;
-    }
-}
+} 
+?>

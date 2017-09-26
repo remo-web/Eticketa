7a28f5cb6528b3b9b36f826bd6686115f7c92ed8
agabiabreu <gabriela.abreu88@gmail.com>
2017-09-26 17:54:37 -0300

    num guento mais

--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -1,96 +1,105 @@
 <?php
-$error = "";
-$nome = $_POST["o_rotulos-nome"];
+ini_set('display_errors', 1);
+$nome = filterInput($_POST["o_rotulos-nome"]);
 //email
 if (empty($_POST["o_rotulos-email"])) {
     $error .= "Email is required ";
 } else {
-    $email = $_POST["o_rotulos-email"];
+    $email = filterInput($_POST["o_rotulos-email"]);
 }
-$empresa = $_POST["o_rotulos-empresa"];
-$telefone = $_POST["o_rotulos-telefone"];
-$largura = $_POST["o_rotulos-largura"];
-$altura = $_POST["o_rotulos-altura"];
-$formato = $_POST["o_rotulos-formato"];
-$quantidade = $_POST["o_rotulos-quantidade"];
-$frente = $_POST["o_rotulos-frente"];
-$verso = $_POST["o_rotulos-verso"];
-$finalidade = $_POST["o_rotulos-finalidade"];
-$mensagem = $_POST["o_rotulos-mensagem"];
+$empresa = filterInput($_POST["o_rotulos-empresa"]);
+$telefone = filterInput($_POST["o_rotulos-telefone"]);
+$largura = filterInput($_POST["o_rotulos-largura"]);
+$altura = filterInput($_POST["o_rotulos-altura"]);
+$formato = filterInput($_POST["o_rotulos-formato"]);
+$quantidade = filterInput($_POST["o_rotulos-quantidade"]);
+$frente = filterInput($_POST["o_rotulos-frente"]);
+$verso = filterInput($_POST["o_rotulos-verso"]);
+$finalidade = filterInput($_POST["o_rotulos-finalidade"]);
+$obs = filterInput($_POST["o_rotulos-mensagem"]);
 
 $To = "raphael.pais@eticketa.com.br";
 $uglySubject = "[Site | Orçamento] Rótulos";
 $Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
  
-// prepare email body text
-$Body .= "Nome: ";
-$Body .= $nome;
-$Body .= "\n";
+echo $email.$To.$obs.$Subject;
+if(sendEmailWithAttachments($To,$email,$Subject,$obs))
+{
+    echo "Email sent successfully!";
+}
+else
+{
+    echo "Failed to send the email...";
+}
  
-$Body .= "E-mail: ";
-$Body .= $email;
-$Body .= "\n";
+function filterInput($data)
+{
+     $data = trim($data);
+     $data = stripslashes($data);
+     $data = htmlspecialchars($data);
+     return $data;
+}
+function sendEmailWithAttachments($To,$email,$Subject,$obs){
  
-$Body .= "Cargo / Empresa: ";
-$Body .= $empresa;
-$Body .= "\n";
+     
+    if(isset($_FILES)) {
+echo "iamhere";
+        $allowedExtensions = array("pdf","doc","docx","gif","jpeg","jpg","JPG","png","PNG","rtf","txt","xml");
  
-$Body .= "Telefone: ";
-$Body .= $telefone;
-$Body .= "\n";
+        $files = array();
+        foreach($_FILES as $name=>$file) {
+            //die("file size: ".$file['size']);
+            if($file['size']>=5242880)//5mb
+            {
+                $fileSize=$file['size'];
+                return false;
+            }
+            $file_name = $file['name']; 
+            $temp_name = $file['tmp_name'];
+            $file_type = $file['type'];
+            $path_parts = pathinfo($file_name);
+            $ext = $path_parts['extension'];
+            if(!in_array($ext,$allowedExtensions)) {
+                return false;
+                die("File $file_name has the extensions $ext which is not allowed");
+            }
  
-$Body .= "Largura: ";
-$Body .= $largura;
-$Body .= " cm";
-$Body .= "\n";
+            //move the file to the server, cannot be skipped
+            $server_file="/tmp/$path_parts[basename]";
+            move_uploaded_file($temp_name,$server_file);
+            array_push($files,$server_file);
+            //array_push($files,$temp_name);
+        }
  
-$Body .= "Altura: ";
-$Body .= $altura;
-$Body .= " cm";
-$Body .= "\n";
+        // email fields
+        $headers = "From: $email";
  
-$Body .= "Formato: ";
-$Body .= $formato;
-$Body .= "\n";
  
-$Body .= "Quantidade: ";
-$Body .= $quantidade;
-$Body .= "\n";
+        // boundary 
+        $semi_rand = md5(time()); 
+        $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x"; 
  
-$Body .= "Frente: ";
-$Body .= $frente;
-$Body .= " cores";
-$Body .= "\n";
+        // headers for attachment 
+        $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\""; 
  
-$Body .= "Verso: ";
-$Body .= $verso;
-$Body .= " cores";
-$Body .= "\n";
+        // multipart boundary 
+        $message = "This is a multi-part message in MIME format.\n\n" . "--{$mime_boundary}\n" . "Content-Type: text/plain; charset=\"iso-8859-1\"\n" . "Content-Transfer-Encoding: 7bit\n\n" . $message . "\n\n"; 
+        $message .= "--{$mime_boundary}\n";
  
-$Body .= "Finalidade: ";
-$Body .= $finalidade;
-$Body .= "\n";
+        // preparing attachments
+        for($x=0;$x<count($files);$x++){
+            $file = fopen($files[$x],"rb");
+            $data = fread($file,filesize($files[$x]));
+            fclose($file);
+            $data = chunk_split(base64_encode($data));
+            $message .= "Content-Type: {\"application/octet-stream\"};\n" . " name=\"$files[$x]\"\n" . 
+            "Content-Disposition: attachment;\n" . " filename=\"$files[$x]\"\n" . 
+            "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
+            $message .= "--{$mime_boundary}\n";
+        }
  
-$Body .= "Observações: ";
-$Body .= $mensagem;
-$Body .= "\n";
-
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
-$headers .= "Content-Type: text/plain; charset=UTF-8" . "\r\n";
-$headers .= "From: $email" . "\r\n";
+        // send
+        return @mail($To, $Subject, $message, $headers);
  
-// send email
-$success = mail($To, $Subject, $Body, $headers);
- 
-// redirect to success page
-if ($success && $error == ""){
-    echo "success";
-} else {
-    if($error == ""){
-        echo "Algo deu errado... Mas deu errado num nível, que é melhor você nos ligar no telefone (21) 3490-9292, porque pelo site vai ser difícil.";
-    } else {
-        echo $error;
     }
-} 
-?>
+}   

5313c480d7dc6ada6a34c9f6956f64607a7e2dba
agabiabreu <gabriela.abreu88@gmail.com>
2017-09-26 15:18:31 -0300

    mais teste

--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -18,11 +18,26 @@ $verso = $_POST["o_rotulos-verso"];
 $finalidade = $_POST["o_rotulos-finalidade"];
 $mensagem = $_POST["o_rotulos-mensagem"];
 
+$arquivo = isset($_FILES["file_attach"]) ? $_FILES["file_attach"] : FALSE; 
+ 
+if(file_exists($arquivo["tmp_name"]) and !empty($arquivo)){ 
+ 
+$fp = fopen($_FILES["file_attach"]["tmp_name"],"rb"); 
+$anexo = fread($fp,filesize($_FILES["file_attach"]["tmp_name"])); 
+$anexo = base64_encode($anexo); 
+ 
+fclose($fp); 
+ 
+$anexo = chunk_split($anexo); 
+ 
+ 
+$boundary = "XYZ-" . date("dmYis") . "-ZYX"; 
+
 $To = "raphael.pais@eticketa.com.br";
 $uglySubject = "[Site | Orçamento] Rótulos";
 $Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
  
-// prepare email body text
+/* prepare email body text
 $Body .= "Nome: ";
 $Body .= $nome;
 $Body .= "\n";
@@ -75,6 +90,8 @@ $Body .= "Observações: ";
 $Body .= $mensagem;
 $Body .= "\n";
 
+
+
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
 $headers .= "Content-Type: text/plain; charset=UTF-8" . "\r\n";
@@ -92,5 +109,45 @@ if ($success && $error == ""){
     } else {
         echo $error;
     }
+} */
+
+$mens = "--$boundary" . $quebra_linha . ""; 
+$mens .= "Content-Transfer-Encoding: 8bits" . $quebra_linha . ""; 
+$mens .= "Content-Type: text/html; charset=\"ISO-8859-1\"" . $quebra_linha . "" . $quebra_linha . ""; //plain 
+$mens .= "$mensagem" . $quebra_linha . ""; 
+$mens .= "--$boundary" . $quebra_linha . ""; 
+$mens .= "Content-Type: ".$arquivo["type"]."" . $quebra_linha . ""; 
+$mens .= "Content-Disposition: attachment; filename=\"".$arquivo["name"]."\"" . $quebra_linha . ""; 
+$mens .= "Content-Transfer-Encoding: base64" . $quebra_linha . "" . $quebra_linha . ""; 
+$mens .= "$anexo" . $quebra_linha . ""; 
+$mens .= "--$boundary--" . $quebra_linha . ""; 
+ 
+$headers = "MIME-Version: 1.0" . $quebra_linha . ""; 
+$headers .= "From: $email " . $quebra_linha . ""; 
+$headers .= "Return-Path: $email " . $quebra_linha . ""; 
+$headers .= "Content-type: multipart/mixed; boundary=\"$boundary\"" . $quebra_linha . ""; 
+$headers .= "$boundary" . $quebra_linha . ""; 
+ 
+ 
+//envio o email com o anexo 
+mail($To,$Subject,$mens,$headers, "-r".$email); 
+ 
+echo"Email enviado com Sucesso!"; 
+ 
+} 
+ 
+//se nao tiver anexo 
+else{ 
+ 
+$headers = "MIME-Version: 1.0" . $quebra_linha . ""; 
+$headers .= "Content-type: text/html; charset=iso-8859-1" . $quebra_linha . ""; 
+$headers .= "From: $email " . $quebra_linha . ""; 
+$headers .= "Return-Path: $email " . $quebra_linha . ""; 
+ 
+//envia o email sem anexo 
+mail($To,$Subject,$mensagem, $headers, "-r".$email); 
+ 
+ 
+echo"Email enviado com Sucesso!"; 
 } 
 ?>

7ac28edb8133a4e1e758b966118ecff5e6f8ffc4
agabiabreu <gabriela.abreu88@gmail.com>
2017-09-28 13:41:08 -0300

    Teste using the real phpmailer

--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -1,75 +1,26 @@
 <?php
-/**
- * PHPMailer simple contact form example.
- * If you want to accept and send uploads in your form, look at the send_file_upload example.
- */
-//Import the PHPMailer class into the global namespace
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 
-require './PHPMailer/src/Exception.php';
-require './PHPMailer/src/PHPMailer.php';
-require './PHPMailer/src/SMTP.php';
-require './PHPMailer/compose.json';
-if (array_key_exists('to', $_POST)) {
-    $err = false;
-    $msg = '';
-    $email = '';
-    //Apply some basic validation and filtering to the subject
-    if (array_key_exists('o_rotulos-empresa', $_POST)) {
-        $subject = substr(strip_tags($_POST['o_rotulos-empresa']), 0, 255);
-    } else {
-        $subject = 'No subject given';
-    }
-    //Apply some basic validation and filtering to the query
-    if (array_key_exists('query', $_POST)) {
-        //Limit length and strip HTML tags
-        $query = substr(strip_tags($_POST['query']), 0, 16384);
-    } else {
-        $query = '';
-        $msg = 'No query provided!';
-        $err = true;
-    }
-    //Apply some basic validation and filtering to the name
-    if (array_key_exists('o_rotulos-nome', $_POST)) {
-        //Limit length and strip HTML tags
-        $name = substr(strip_tags($_POST['o_rotulos-nome']), 0, 255);
-    } else {
-        $name = '';
-    }
-    //Validate to address
-    //Never allow arbitrary input for the 'to' address as it will turn your form into a spam gateway!
-    //Substitute appropriate addresses from your own domain, or simply use a single, fixed address
-    if (array_key_exists('to', $_POST) and in_array($_POST['to'], ['sales', 'support', 'accounts'])) {
-        $to = $_POST['to'] . 'raphael.pais@eticketa.com.br';
-    } else {
-        $to = 'raphael.pais@eticketa.com.br';
-    }
-    //Make sure the address they provided is valid before trying to use it
-    if (array_key_exists('o_rotulos-email', $_POST) and PHPMailer::validateAddress($_POST['o_rotulos-email'])) {
-        $email = $_POST['o_rotulos-email'];
-    } else {
-        $msg .= "Error: invalid email address provided";
-        $err = true;
-    }
-    if (!$err) {
-        $mail = new PHPMailer;
-        $mail->isSMTP();
-        $mail->Host = 'localhost';
-        $mail->Port = 2500;
-        $mail->CharSet = 'utf-8';
-        //It's important not to use the submitter's address as the from address as it's forgery,
-        //which will cause your messages to fail SPF checks.
-        //Use an address in your own domain as the from address, put the submitter's address in a reply-to
-        $mail->setFrom('contact@example.com', (empty($name) ? 'Contact form' : $name));
-        $mail->addAddress($to);
-        $mail->addReplyTo($email, $name);
-        $mail->Subject = 'Contact form: ' . $subject;
-        $mail->Body = "Contact form submission\n\n" . $query;
-        if (!$mail->send()) {
-            $msg .= "Mailer Error: " . $mail->ErrorInfo;
-        } else {
-            $msg .= "Message sent!";
+require_once './vendor/autoload.php'
+    
+if(isset($_POST["email"])){
+    if(!filter_var($_POST['email']), FILTER_VALIDATED_EMAIL){
+        echo "Digite um email válido"
+    }else{
+        
+        $mail = new PHPMailer();
+        $mail->setFrom($_POST['o_rotulos-email'],$_POST['o_rotulos-nome']);
+        $mail->addAddress("raphael.pais@eticketa.com.br","Teste")
+        $mail->isHTML(true);
+        $mail->Subject = "[Site | Orçamento] Rótulos";
+        $mail->Body = "<h4> Nome: ".$_POST["o_rotulos-nome"]."Email: ".$_POST["o_rotulos-email"]."Empresa: ".$_POST["o_rotulos-empresa"]."Telefone: ".$_POST["o_rotulos-telefone"]."Largura: ".$_POST["o_rotulos-largura"]."Altura: ".$_POST["o_rotulos-altura"]."Formato: ".$_POST["o_rotulos-formato"]."Quantidade: ".$_POST["o_rotulos-quantidade"]."Frente: ".$_POST["o_rotulos-frente"]."Verso: ".$_POST["o_rotulos-verso"]."Finalidade: ".$_POST["o_rotulos-finalidade"]."Observações: ".$_POST["o_rotulos-mensagem"]."<h4>";
+        
+        if($mail->send()){
+            echo "Email enviado";
+        }else{
+            echo "error"
         }
     }
-} ?>
+    
+}
+
+?>

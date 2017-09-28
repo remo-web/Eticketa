a09c68d539cfb56f1d6d888c4574e43733306a26
agabiabreu <gabriela.abreu88@gmail.com>
2017-09-28 13:50:48 -0300

    com uns ;

--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -1,23 +1,29 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-require_once './vendor/autoload.php'
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
+require_once './vendor/autoload.php';
     
 if(isset($_POST["email"])){
     if(!filter_var($_POST['email']), FILTER_VALIDATED_EMAIL){
-        echo "Digite um email válido"
+        echo "Digite um email válido";
     }else{
         
-        $mail = new PHPMailer();
-        $mail->setFrom($_POST['o_rotulos-email'],$_POST['o_rotulos-nome']);
-        $mail->addAddress("raphael.pais@eticketa.com.br","Teste")
+        $mail = new PHPMailer(true);
+        $mail->setFrom("gabriela.abreu88@gmail.com", "Gostosa");
+        $mail->addAddress("raphael.pais@eticketa.com.br","Teste");
+        $mail->addAttachment('/img/gabi.jpg');
         $mail->isHTML(true);
         $mail->Subject = "[Site | Orçamento] Rótulos";
-        $mail->Body = "<h4> Nome: ".$_POST["o_rotulos-nome"]."Email: ".$_POST["o_rotulos-email"]."Empresa: ".$_POST["o_rotulos-empresa"]."Telefone: ".$_POST["o_rotulos-telefone"]."Largura: ".$_POST["o_rotulos-largura"]."Altura: ".$_POST["o_rotulos-altura"]."Formato: ".$_POST["o_rotulos-formato"]."Quantidade: ".$_POST["o_rotulos-quantidade"]."Frente: ".$_POST["o_rotulos-frente"]."Verso: ".$_POST["o_rotulos-verso"]."Finalidade: ".$_POST["o_rotulos-finalidade"]."Observações: ".$_POST["o_rotulos-mensagem"]."<h4>";
+        $mail->Body = "<h4> Só testando <h4>";
         
         if($mail->send()){
             echo "Email enviado";
         }else{
-            echo "error"
+            echo "error";
         }
     }
     

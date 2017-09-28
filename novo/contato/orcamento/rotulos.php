126a63914ccc9e62317be99bbe6dbb938844bd7c
agabiabreu <gabriela.abreu88@gmail.com>
2017-09-28 14:33:53 -0300

    volta

--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -1,26 +1,96 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+$error = "";
+$nome = $_POST["o_rotulos-nome"];
+//email
+if (empty($_POST["o_rotulos-email"])) {
+    $error .= "Email is required ";
+} else {
+    $email = $_POST["o_rotulos-email"];
+}
+$empresa = $_POST["o_rotulos-empresa"];
+$telefone = $_POST["o_rotulos-telefone"];
+$largura = $_POST["o_rotulos-largura"];
+$altura = $_POST["o_rotulos-altura"];
+$formato = $_POST["o_rotulos-formato"];
+$quantidade = $_POST["o_rotulos-quantidade"];
+$frente = $_POST["o_rotulos-frente"];
+$verso = $_POST["o_rotulos-verso"];
+$finalidade = $_POST["o_rotulos-finalidade"];
+$mensagem = $_POST["o_rotulos-mensagem"];
 
-require './PHPMailer/src/Exception.php';
-require './PHPMailer/src/PHPMailer.php';
-require './PHPMailer/src/SMTP.php';
-require_once './vendor/autoload.php';
-    
-    
-        $mail = new PHPMailer(true);
-        $mail->setFrom("gabriela.abreu88@gmail.com", "Gostosa");
-        $mail->addAddress("raphael.pais@eticketa.com.br","Teste");
-        $mail->addAttachment('/img/gabi.jpg');
-        $mail->isHTML(true);
-        $mail->Subject = "[Site | Orçamento] Rótulos";
-        $mail->Body = "<h4> Só testando <h4>";
-        
-        if($mail->send()){
-            echo "Email enviado";
-        }else{
-            echo "error";
-        }
-    
+$To = "raphael.pais@eticketa.com.br";
+$uglySubject = "[Site | Orçamento] Rótulos";
+$Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
+ 
+// prepare email body text
+$Body .= "Nome: ";
+$Body .= $nome;
+$Body .= "\n";
+ 
+$Body .= "E-mail: ";
+$Body .= $email;
+$Body .= "\n";
+ 
+$Body .= "Cargo / Empresa: ";
+$Body .= $empresa;
+$Body .= "\n";
+ 
+$Body .= "Telefone: ";
+$Body .= $telefone;
+$Body .= "\n";
+ 
+$Body .= "Largura: ";
+$Body .= $largura;
+$Body .= " cm";
+$Body .= "\n";
+ 
+$Body .= "Altura: ";
+$Body .= $altura;
+$Body .= " cm";
+$Body .= "\n";
+ 
+$Body .= "Formato: ";
+$Body .= $formato;
+$Body .= "\n";
+ 
+$Body .= "Quantidade: ";
+$Body .= $quantidade;
+$Body .= "\n";
+ 
+$Body .= "Frente: ";
+$Body .= $frente;
+$Body .= " cores";
+$Body .= "\n";
+ 
+$Body .= "Verso: ";
+$Body .= $verso;
+$Body .= " cores";
+$Body .= "\n";
+ 
+$Body .= "Finalidade: ";
+$Body .= $finalidade;
+$Body .= "\n";
+ 
+$Body .= "Observações: ";
+$Body .= $mensagem;
+$Body .= "\n";
 
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8" . "\r\n";
+$headers .= "From: $email" . "\r\n";
+ 
+// send email
+$success = mail($To, $Subject, $Body, $headers);
+ 
+// redirect to success page
+if ($success && $error == ""){
+    echo "success";
+} else {
+    if($error == ""){
+        echo "Algo deu errado... Mas deu errado num nível, que é melhor você nos ligar no telefone (21) 3490-9292, porque pelo site vai ser difícil.";
+    } else {
+        echo $error;
+    }
+} 
 ?>

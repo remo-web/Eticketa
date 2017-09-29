9fa2db3e22f66cc6dc357beb8bbee0bb5060e06f
agabiabreu <gabriela.abreu88@gmail.com>
2017-09-29 12:18:35 -0300

    teste pasta upload

---
+++ novo/contato/orcamento/upload/teste.php
@@ -0,0 +1,116 @@
+<?php
+$error = "";
+if(isset($_POST['o_rotulos-enviar']) && $_POST['o_rotulos-enviar']=='etka_o-rotulos'):
+    $arquivo = $_FILES['o_rotulos-anexo'];
+    $nome = $arquivo['name'];
+    $tmp = $arquivo['tmp_name'];
+    
+    $extensao = explode('.',$nome);
+    $ext = end($extensao);
+   
+    $novonome = md5($nome).'.'.$ext;
+    
+    if(empty($arquivo)):
+        echo 'Selecione um arquivo para upload';
+    else:
+        move_uploaded_file($tmp, 'upload/'.$novonome);
+
+    endif;
+endif;
+$nome = $_POST["o_rotulos-nome"];
+//email
+if (empty($_POST["o_rotulos-email"])) {
+    $error .= "Email is required ";
+} else {
+    $email = $_POST["o_rotulos-email"];
+}
+$empresa = $_POST["o_rotulos-empresa"];
+$telefone = $_POST["o_rotulos-telefone"];
+$largura = $_POST["o_rotulos-largura"];
+$altura = $_POST["o_rotulos-altura"];
+$formato = $_POST["o_rotulos-formato"];
+$quantidade = $_POST["o_rotulos-quantidade"];
+$frente = $_POST["o_rotulos-frente"];
+$verso = $_POST["o_rotulos-verso"];
+$finalidade = $_POST["o_rotulos-finalidade"];
+$mensagem = $_POST["o_rotulos-mensagem"];
+
+$To = "raphael.pais@eticketa.com.br";
+$uglySubject = "[Site | Orçamento] Rótulos";
+$Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
+
+ 
+// prepare email body text
+$Body .= "Nome: ";
+$Body .= $nome;
+$Body .= "\n";
+ 
+$Body .= "E-mail: ";
+$Body .= $email;
+$Body .= "\n";
+ 
+$Body .= "Cargo / Empresa: ";
+$Body .= $empresa;
+$Body .= "\n";
+ 
+$Body .= "Telefone: ";
+$Body .= $telefone;
+$Body .= "\n";
+ 
+$Body .= "Largura: ";
+$Body .= $largura;
+$Body .= " cm";
+$Body .= "\n";
+ 
+$Body .= "Altura: ";
+$Body .= $altura;
+$Body .= " cm";
+$Body .= "\n";
+ 
+$Body .= "Formato: ";
+$Body .= $formato;
+$Body .= "\n";
+ 
+$Body .= "Quantidade: ";
+$Body .= $quantidade;
+$Body .= "\n";
+ 
+$Body .= "Frente: ";
+$Body .= $frente;
+$Body .= " cores";
+$Body .= "\n";
+ 
+$Body .= "Verso: ";
+$Body .= $verso;
+$Body .= " cores";
+$Body .= "\n";
+ 
+$Body .= "Finalidade: ";
+$Body .= $finalidade;
+$Body .= "\n";
+ 
+$Body .= "Observações: ";
+$Body .= $mensagem;
+$Body .= "\n";
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8" . "\r\n";
+$headers .= "From: $email" . "\r\n";
+ 
+// send email
+$success = mail($To, $Subject, $Body, $headers);
+//nao tá funcionando ainda:
+
+ 
+// redirect to success page
+if ($success && $error == ""){
+    echo "success";
+} else {
+    if($error == ""){
+        echo "Algo deu errado... Mas deu errado num nível, que é melhor você nos ligar no telefone (21) 3490-9292, porque pelo site vai ser difícil.";
+    } else {
+        echo $error;
+    }
+} 
+?>

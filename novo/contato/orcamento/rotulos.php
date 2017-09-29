082afbb5433e0b135b5b6a965d32afcc272aa4fd
agabiabreu <gabriela.abreu88@gmail.com>
2017-09-29 15:43:59 -0300

    igual ao meu

--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -1,107 +1,40 @@
 <?php
-$error = "";
+
 if(isset($_POST['o_rotulos-enviar']) && $_POST['o_rotulos-enviar']=='etka_o-rotulos'):
-$uploaddir = "upload";
-
-$uploadfile = $uploaddir.$_FILES['o_rotulos-anexo']['name'];
-
-move_uploaded_file($_FILES['o_rotulos-anexo']['tmp_name'],$uploadfile);
-
-$nome = $_POST["o_rotulos-nome"];
-//email
-if (empty($_POST["o_rotulos-email"])) {
-    $error .= "Email is required ";
-} else {
-    $email = $_POST["o_rotulos-email"];
-}
-$empresa = $_POST["o_rotulos-empresa"];
-$telefone = $_POST["o_rotulos-telefone"];
-$largura = $_POST["o_rotulos-largura"];
-$altura = $_POST["o_rotulos-altura"];
-$formato = $_POST["o_rotulos-formato"];
-$quantidade = $_POST["o_rotulos-quantidade"];
-$frente = $_POST["o_rotulos-frente"];
-$verso = $_POST["o_rotulos-verso"];
-$finalidade = $_POST["o_rotulos-finalidade"];
-$mensagem = $_POST["o_rotulos-mensagem"];
-
-$To = "raphael.pais@eticketa.com.br";
-$uglySubject = "[Site | Orçamento] Rótulos";
-$Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
-
- 
-// prepare email body text
-$Body .= "Nome: ";
-$Body .= $nome;
-$Body .= "\n";
- 
-$Body .= "E-mail: ";
-$Body .= $email;
-$Body .= "\n";
- 
-$Body .= "Cargo / Empresa: ";
-$Body .= $empresa;
-$Body .= "\n";
- 
-$Body .= "Telefone: ";
-$Body .= $telefone;
-$Body .= "\n";
- 
-$Body .= "Largura: ";
-$Body .= $largura;
-$Body .= " cm";
-$Body .= "\n";
- 
-$Body .= "Altura: ";
-$Body .= $altura;
-$Body .= " cm";
-$Body .= "\n";
- 
-$Body .= "Formato: ";
-$Body .= $formato;
-$Body .= "\n";
- 
-$Body .= "Quantidade: ";
-$Body .= $quantidade;
-$Body .= "\n";
- 
-$Body .= "Frente: ";
-$Body .= $frente;
-$Body .= " cores";
-$Body .= "\n";
- 
-$Body .= "Verso: ";
-$Body .= $verso;
-$Body .= " cores";
-$Body .= "\n";
- 
-$Body .= "Finalidade: ";
-$Body .= $finalidade;
-$Body .= "\n";
- 
-$Body .= "Observações: ";
-$Body .= $mensagem;
-$Body .= "\n";
-
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
-$headers .= "Content-Type: text/plain; charset=UTF-8" . "\r\n";
-$headers .= "From: $email" . "\r\n";
- 
-// send email
-$success = mail($To, $Subject, $Body, $headers);
-//nao tá funcionando ainda:
-
- endif;
-// redirect to success page
-if ($success && $error == ""){
-    echo "success";
-} else {
-    if($error == ""){
-        echo "Algo deu errado... Mas deu errado num nível, que é melhor você nos ligar no telefone (21) 3490-9292, porque pelo site vai ser difícil.";
-    } else {
-        echo $error;
-    }
-} 
-
+    $arquivo = $_FILES['etka_o-anexo'];
+    $nome = $_POST['nome'];
+    
+    $para = "raphael.pais@eticketa.com.br";
+    
+    $boundary = "XYZ-".date("dmYis")."-ZYX";
+    $fp = fopen($arquivo["tmp_name"], "rb"); // abre o arquivo enviado
+    $anexo = fread($fp, filesize($arquivo["tmp_name"])); // calcula o tamanho
+    $anexo = base64_encode($anexo); // codifica o anexo em base 64
+    fclose($fp); // fecha o arquivo
+    
+    // cabeçalho do email
+    $headers  = "MIME-Version: 1.0\n";
+    $headers .= "Content-Type: multipart/mixed; ";
+    //$headers .= "";
+    $headers .= "$boundary\n";
+    
+    // email
+    $mensagem  = "--$boundary\n";
+    $mensagem .= "Content-Type: text/html; charset='utf-8'\n";
+    $mensagem .= "<strong>Nome: </strong> $nome \r\n";
+    $mensagem .= "--$boundary \n";
+    
+    // anexo
+    $mensagem .= "Content-Type: ".$arquivo["type"].";name=".$arquivo['name']." \n";
+    $mensagem .= "Content-Transfer-Encoding: base64 \n";
+    $mensagem .= "Content-Disposition: attachment; filename=".$arquivo['name']." \r\n";
+    $mensagem .= "$anexo \n";
+    $mensagem .= "--$boundary \n";
+    
+    // enviar o email
+    mail($para, $assunto, $mensagem, $headers);
+    echo"Email enviado com Sucesso!";
+    else:
+        echo 'Email não enviado!';
+    endif;
 ?>

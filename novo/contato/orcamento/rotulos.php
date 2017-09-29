73c2c6795f627777fe4ed5d4caa7311fec391766
agabiabreu <gabriela.abreu88@gmail.com>
2017-09-29 17:03:35 -0300

    anexo vazio,sera?

--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -1,38 +1,117 @@
 <?php
+$error = "";
+$nome = $_POST["o_rotulos-nome"];
+//email
+if (empty($_POST["o_rotulos-email"])) {
+    $error .= "Email is required ";
+} else {
+    $email = $_POST["o_rotulos-email"];
+}
+$empresa = $_POST["o_rotulos-empresa"];
+$telefone = $_POST["o_rotulos-telefone"];
+$largura = $_POST["o_rotulos-largura"];
+$altura = $_POST["o_rotulos-altura"];
+$formato = $_POST["o_rotulos-formato"];
+$quantidade = $_POST["o_rotulos-quantidade"];
+$frente = $_POST["o_rotulos-frente"];
+$verso = $_POST["o_rotulos-verso"];
+$finalidade = $_POST["o_rotulos-finalidade"];
+$mensagem = $_POST["o_rotulos-mensagem"];
+$arquivo = $_FILES["o_rotulos-anexo"];
 
-if(isset($_POST['o_rotulos-enviar']) && $_POST['o_rotulos-enviar']=='etka_o-rotulos'):
-    $arquivo = $_FILES['etka_o-anexo'];
-    $nome = $_POST['o_rotulos-nome'];
-    
-    $para = "raphael.pais@eticketa.com.br";
-    
-    $boundary = "XYZ-".date("dmYis")."-ZYX";
-    $fp = fopen($arquivo["tmp_name"], "rb"); // abre o arquivo enviado
-    $anexo = fread($fp, filesize($arquivo["tmp_name"])); // calcula o tamanho
-    $anexo = base64_encode($anexo); // codifica o anexo em base 64
-    fclose($fp); // fecha o arquivo
-    
-    // cabeçalho do email
-    $headers  = "MIME-Version: 1.0\n";
-    $headers .= "Content-Type: multipart/mixed; ";
-    //$headers .= "";
-    $headers .= "$boundary\n";
-    
-    // email
-    $mensagem  = "--$boundary\n";
-    $mensagem .= "Content-Type: text/html; charset='utf-8'\n";
-    $mensagem .= "<strong>Nome: </strong> $nome \r\n";
-    $mensagem .= "--$boundary \n";
-    
-    // anexo
-    $mensagem .= "Content-Type: ".$arquivo["type"].";name=".$arquivo['name']." \n";
-    $mensagem .= "Content-Transfer-Encoding: base64 \n";
-    $mensagem .= "Content-Disposition: attachment; filename=".$arquivo['name']." \r\n";
-    $mensagem .= "$anexo \n";
-    $mensagem .= "--$boundary \n";
-    
-    // enviar o email
-$success = mail($para, $assunto, $mensagem, $headers);
+$boundary = "XYZ-".date("dmYis")."-ZYX";
+
+
+$fp = fopen($arquivo["tmp_name"], "rb"); // abre o arquivo enviado
+
+$anexo = fread($fp, filesize($arquivo["tmp_name"])); // calcula o tamanho
+
+$anexo = base64_encode($anexo);// codifica o anexo em base 64
+
+fclose($fp);// fecha o arquivo
+
+
+
+$To = "raphael.pais@eticketa.com.br";
+$uglySubject = "[Site | Orçamento] Rótulos";
+$Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
+ 
+// prepare email body text
+$Body .= "Nome: ";
+$Body .= $nome;
+$Body .= "\n";
+ 
+$Body .= "E-mail: ";
+$Body .= $email;
+$Body .= "\n";
+ 
+$Body .= "Cargo / Empresa: ";
+$Body .= $empresa;
+$Body .= "\n";
+ 
+$Body .= "Telefone: ";
+$Body .= $telefone;
+$Body .= "\n";
+ 
+$Body .= "Largura: ";
+$Body .= $largura;
+$Body .= " cm";
+$Body .= "\n";
+ 
+$Body .= "Altura: ";
+$Body .= $altura;
+$Body .= " cm";
+$Body .= "\n";
+ 
+$Body .= "Formato: ";
+$Body .= $formato;
+$Body .= "\n";
+ 
+$Body .= "Quantidade: ";
+$Body .= $quantidade;
+$Body .= "\n";
+ 
+$Body .= "Frente: ";
+$Body .= $frente;
+$Body .= " cores";
+$Body .= "\n";
+ 
+$Body .= "Verso: ";
+$Body .= $verso;
+$Body .= " cores";
+$Body .= "\n";
+ 
+$Body .= "Finalidade: ";
+$Body .= $finalidade;
+$Body .= "\n";
+ 
+$Body .= "Observações: ";
+$Body .= $mensagem;
+$Body .= "\n";
+
+$headers  = "MIME-Version: 1.0 \r\n";
+$headers .= "Content-Type: multipart/mixed \r\n";
+//$headers .= "boundary="$boundary" \r\n";
+$headers .= "$boundary \r\n";
+$headers .= "From: $email" . "\r\n";
+
+// email
+
+$Body  = "--$boundary \r\n";
+$Body .= "Content-Type: text/html; charset='utf-8' \r\n";
+$Body .= "Nome: $nome \r\n";
+$Body .= "--$boundary \r\n";
+
+// anexo 
+$Body .= "Content-Type: ".$arquivo["type"]."; name=\"".$arquivo['name']."\" \r\n"; 
+$Body .= "Content-Transfer-Encoding: base64 \r\n"; 
+$Body .= "Content-Disposition: attachment; filename=\"".$arquivo['name']."\" \r\n";
+$Body .= "$anexo \n"; 
+$Body .= "--$boundary \r\n"; 
+
+ 
+// send email
+$success = mail($To, $Subject, $Body, $headers);
  
 // redirect to success page
 if ($success && $error == ""){
@@ -44,5 +123,4 @@ if ($success && $error == ""){
         echo $error;
     }
 } 
-
 ?>

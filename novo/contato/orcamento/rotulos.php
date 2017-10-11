5af911eb64a2f01fbc06f95744e89fe3bb65bce2
agabiabreu <gabriela.abreu88@gmail.com>
2017-10-11 14:27:03 -0300

    nome da pasta

--- novo/contato/orcamento/rotulos.php
+++ novo/contato/orcamento/rotulos.php
@@ -18,7 +18,7 @@ $verso = $_POST["o_rotulos-verso"];
 $finalidade = $_POST["o_rotulos-finalidade"];
 $mensagem = $_POST["o_rotulos-mensagem"];
 
-$uploaddir = '/upload/';
+$uploaddir = 'upload/';
 $uploadfile = $uploaddir . basename($_FILES['o_rotulos-anexo']['name']);
 move_uploaded_file($_FILES['o_rotulos-anexo']['tmp_name'], $uploadfile);
 print_r($_FILES);

d081bb9ff548c09d7148a7f4a1cbd1cb93bef3c4
Raphael <raphaelrpais@gmail.com>
2017-10-16 14:05:56 -0200

    rezando pra voltar tudo pro lugar

---
+++ novo/contato/orcamento/rotulos.js
@@ -0,0 +1,66 @@
+$("#etka_o-rotulos").submit(function(event){
+    var email = document.forms["etka_o-rotulos"]["o_rotulos-email"].value;
+    var o_rotulos_email = document.getElementById("o_rotulos-email");
+    var mensagem = document.forms["etka_o-rotulos"]["o_rotulos-mensagem"].value;
+    var o_rotulos_msg = document.getElementById("o_rotulos-mensagem");
+    var att = document.createAttribute("required");
+    if (email == "") {
+        o_rotulos_email.setAttributeNode(att);
+        return false;
+        // handle the invalid form...
+        o_rotulosError();
+    }
+    if (mensagem == "") {
+        o_rotulos_msg.setAttributeNode(att);
+        return false;
+        // handle the invalid form...
+        o_rotulosError();
+    } 
+    if (event.isDefaultPrevented()) {
+        // handle the invalid form...
+        o_rotulosError();
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        submitO_rotulos();
+    }
+});
+
+function submitO_rotulos(){
+    // Initiate Variables With Form Content
+    var nome = $("#o_rotulos-nome").val();
+    var email = $("#o_rotulos-email").val();
+    var empresa = $("#o_rotulos-empresa").val();
+    var telefone = $("#o_rotulos-telefone").val();
+    var largura = $("#o_rotulos-largura").val();
+    var altura = $("#o_rotulos-altura").val();
+    var formato = $("input[name=o_rotulos-formato]:checked").val();
+    var quantidade = $("#o_rotulos-quantidade").val();
+    var frente = $("#o_rotulos-frente").val();
+    var verso = $("#o_rotulos-verso").val();
+    var finalidade = $("#o_rotulos-finalidade").val();
+    var mensagem = $("#o_rotulos-mensagem").val();
+ 
+    $.ajax({
+        type: "POST",
+        url: "./contato/orcamento/rotulos.php",
+        data: "o_rotulos-nome=" + nome + "&o_rotulos-email=" + email + "&o_rotulos-empresa=" + empresa + "&o_rotulos-telefone=" + telefone + "&o_rotulos-largura=" + largura + "&o_rotulos-altura=" + altura + "&o_rotulos-formato=" + formato + "&o_rotulos-quantidade=" + quantidade + "&o_rotulos-frente=" + frente + "&o_rotulos-verso=" + verso + "&o_rotulos-finalidade=" + finalidade + "&o_rotulos-mensagem=" + mensagem,
+        success : function(text){
+            if (text == "success"){
+                o_rotulosSuccess();
+            } else {
+                o_rotulosError();
+            }
+        }
+    });
+}
+
+function o_rotulosSuccess(){
+    $( "#etka_o-rotulos-enviado" ).removeClass( "etka_o-rotulos-enviado" );
+    $( '#o_rotulos-nome, #o_rotulos-email, #o_rotulos-empresa, #o_rotulos-telefone, #o_rotulos-largura, #o_rotulos-altura, #o_rotulos-formato, #o_rotulos-quantidade, #o_rotulos-frente, #o_rotulos-verso, #o_rotulos-finalidade, #o_rotulos-mensagem' ).val('');
+}
+
+function o_rotulosError(){
+    $( "#etka_o-rotulos-erro" ).removeClass( "etka_o-rotulos-erro" );
+}
+
---
+++ novo/contato/orcamento/rotulos.php
@@ -0,0 +1,69 @@
+<?php
+
+$error = "";
+
+$nome = $_POST["o_rotulos-nome"];
+
+//email
+if (empty($_POST["o_rotulos-email"])) {
+    $error .= "Email is required ";
+} else {
+    $email = $_POST["o_rotulos-email"];
+}
+
+$empresa = $_POST["o_rotulos-empresa"];
+$telefone = $_POST["o_rotulos-telefone"];
+$largura = $_POST["o_rotulos-largura"];
+$altura = $_POST["o_rotulos-altura"];
+$formato = $_POST["o_rotulos-formato"];
+$quantidade = $_POST["o_rotulos-quantidade"];
+$frente = $_POST["o_rotulos-frente"];
+$verso = $_POST["o_rotulos-verso"];
+$finalidade = $_POST["o_rotulos-finalidade"];
+
+//mensagem
+if (empty($_POST["o_rotulos-mensagem"])) {
+    $error .= "Mensagem is required ";
+} else {
+    $mensagem = $_POST["o_rotulos-mensagem"];
+}
+ 
+$To = "raphael.pais@eticketa.com.br";
+$uglySubject = "[Site | Orçamento] Rótulos para $finalidade";
+$Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
+
+$Body .= "
+<html>
+<body style='width: 690px; font-family: sans-serif'>
+<b style='text-transform: uppercase'>$nome</b><br>
+<b>Cargo/Empresa: </b>$empresa<br>
+<b>E-mail: </b>$email<br>
+<b>Telefone: </b>$telefone<br>
+<b>Dimensões: </b>$largura (L) <b>x</b> $altura (A)<br>
+<b>Formato: </b>$formato<br>
+<b>Quantidade: </b>$quantidade<br>
+<b>Cores: </b>$frente <b>x</b> $verso<br>
+<b>Finalidade: </b>$finalidade<br>
+<b>Observações: </b>$mensagem
+</body></html>";
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8" . "\r\n";
+$headers .= "From: $email" . "\r\n";
+ 
+// send email
+$success = mail($To, $Subject, $Body, $headers);
+ 
+// redirect to success page
+if ($success && $error == ""){
+   echo "success";
+}else{
+    if($error == ""){
+        echo "Algo deu errado... Mas deu errado num nível, que é melhor você nos ligar no telefone (21) 3490-9292, porque pelo site vai ser difícil.";
+    } else {
+        echo $error;
+    }
+}
+ 
+?>

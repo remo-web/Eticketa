15f2678822675d1183fc51d5b0c4dbabe844e3c0
agabiabreu <gabriela.abreu88@gmail.com>
2017-10-17 14:40:17 -0200

    criação php p-ingresso

---
+++ novo/contato/precos/ingresso.php
@@ -0,0 +1,52 @@
+<?php
+
+$error = "";
+
+$nome = $_POST["p_ingresso-nome"];
+
+//email
+if (empty($_POST["p_ingresso-email"])) {
+    $error .= "Email is required ";
+} else {
+    $email = $_POST["p_ingresso-email"];
+}
+
+$empresa = $_POST["p_ingresso-empresa"];
+$quantidade = $_POST["etka_preco-ingresso_slider-label_input"];
+$endereco = $_POST["p_ingresso-endereco"];
+
+ 
+$To = "raphael.pais@eticketa.com.br";
+$uglySubject = "[Site | Preços] Tickets & Ingressos";
+$Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
+
+$Body .= "
+<html>
+<body style='width: 690px; font-family: sans-serif'>
+<b style='text-transform: uppercase'>$nome</b><br>
+<b>Cargo/Empresa: </b>$empresa<br>
+<b>E-mail: </b>$email<br>
+<b>Quantidade: </b>$quantidade<br>
+<b>Endereço: </b>$endereco
+</body></html>";
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8" . "\r\n";
+$headers .= "From: $email" . "\r\n";
+ 
+// send email
+$success = mail($To, $Subject, $Body, $headers);
+ 
+// redirect to success page
+if ($success && $error == ""){
+   echo "success";
+}else{
+    if($error == ""){
+        echo "Algo deu errado... Mas deu errado num nível, que é melhor você nos ligar no telefone (21) 3490-9292, porque pelo site vai ser difícil.";
+    } else {
+        echo $error;
+    }
+}
+ 
+?>

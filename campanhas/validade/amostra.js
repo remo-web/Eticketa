7c3f938808d96e5e96a874529cb52793714f6480
Raphael <raphaelrpais@gmail.com>
2017-10-23 18:46:51 -0200

    oi

--- campanhas/validade/amostra.js
+++ campanhas/validade/amostra.js
@@ -37,7 +37,7 @@ function submitAValidade(){
  
     $.ajax({
         type: "POST",
-        url: "./campanhas/validade/amostra.php",
+        url: "./amostra.php",
         data: "validade_a-nome=" + nome + "&validade_a-email=" + email + "&validade_a-empresa=" + empresa + "&validade_a-tel=" + telefone + "&validade_a-endereco=" + endereco + "&validade_a-mensagem=" + mensagem,
         success : function(text){
             if (text == "success"){

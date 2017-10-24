eb064e330fdc0871bbe9102c114519b234fbfb9a
agabiabreu <gabriela.abreu88@gmail.com>
2017-10-24 18:53:43 -0200

    pedidos

---
+++ campanhas/_assets/pedido.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+    $(".etka_card__close").click(function() {
+        $(".etka_popup-card").addClass("etka_popup-enviado");
+    });
+});
+
+$("#etka_pedido").submit(function(event){
+    var email = document.forms["etka_pedido"]["pedido-email"].value;
+    var contato_email = document.getElementById("pedido-email");
+    var endereco = document.forms["etka_pedido"]["pedido-endereco"].value;
+    var contato_end = document.getElementById("pedido-endereco");
+    var emailemp = document.forms["etka_pedido"]["pedido-emailemp"].value;
+    var contato_emailemp = document.getElementById("pedido-emailemp");
+    var endemp = document.forms["etka_pedido"]["pedido-endemp"].value;
+    var contato_endemp = document.getElementById("pedido-endemp");
+    var att = document.createAttribute("required");
+    if (email == "") {
+        contato_email.setAttributeNode(att);
+        return false;
+        // handle the invalid form...
+        pedidoError();
+    }
+    if (endereco == "") {
+        contato_end.setAttributeNode(att);
+        return false;
+        // handle the invalid form...
+        pedidoError();
+    } 
+    if (emailemp == "") {
+        contato_emailemp.setAttributeNode(att);
+        return false;
+        // handle the invalid form...
+        pedidoError();
+    }
+    if (endemp == "") {
+        contato_endemp.setAttributeNode(att);
+        return false;
+        // handle the invalid form...
+        pedidoError();
+    } 
+    if (event.isDefaultPrevented()) {
+        // handle the invalid form...
+        pedidoError();
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        submitpedido();
+    }
+});
+
+function submitpedido(){
+    // Initiate Variables With Form Content
+    var assunto = $("#pedido-assunto").val();
+    var nome = $("#pedido-nome").val();
+    var email = $("#pedido-email").val();
+    var empresa = $("#pedido-empresa").val();
+    var telefone = $("#pedido-telefone").val();
+    var endereco = $("#pedido-endereco").val();
+    var quantidade = $("#pedido-quantidade").val();
+    var mensagem = $("#pedido-mensagem").val();
+    var cnpj = $("#pedido-cnpj").val();
+    var razao = $("#pedido-razao").val();
+    var endemp = $("#pedido-endemp").val();
+    var insest = $("#pedido-insest").val();
+    var emailemp = $("#pedido-emailemp").val();
+    var telemp = $("#pedido-telemp").val();
+    var simples = $("#pedido-simples").val();
+ 
+    $.ajax({
+        type: "POST",
+        url: "../_assets/pedido.php",
+        data: "pedido-assunto=" + assunto + "&pedido-nome=" + nome + "&pedido-email=" + email + "&pedido-empresa=" + empresa + "&pedido-telefone=" + telefone + "&pedido-endereco=" + endereco + "&pedido-quantidade=" + quantidade + "&pedido-mensagem=" + mensagem + "&pedido-cnpj=" + cnpj + "&pedido-razao=" + razao + "&pedido-endemp=" + endemp + "&pedido-insest=" + insest + "&pedido-emailemp=" + emailemp + "&pedido-telemp=" + telemp + "&pedido-simples=" + simples,
+        success : function(text){
+            if (text == "success"){
+                pedidoSuccess();
+            } else {
+                pedidoError();
+            }
+        }
+    });
+}
+
+function pedidoSuccess(){
+    $( "#etka_pedido-enviado" ).removeClass( "etka_popup-enviado" );
+    $( '#pedido-assunto, #pedido-nome, #pedido-email, #pedido-empresa, #pedido-telefone, #pedido-endereco, #pedido-quantidade, #pedido-mensagem, #pedido-cnpj, #pedido-razao, #pedido-endemp, #pedido-insest, #pedido-emailemp, #pedido-telemp, #pedido-simples' ).val('');
+}
+
+function pedidoError(){
+    $( "#etka_pedido-erro" ).removeClass( "etka_popup-erro" );
+}
+
---
+++ campanhas/_assets/pedido.php
@@ -0,0 +1,97 @@
+<?php
+
+$error = "";
+
+$assunto = $_POST["pedido-assunto"];
+$nome = $_POST["pedido-nome"];
+
+//email
+if (empty($_POST["pedido-email"])) {
+    $error .= "Email is required ";
+} else {
+    $email = $_POST["pedido-email"];
+}
+
+$empresa = $_POST["pedido-empresa"];
+$telefone = $_POST["pedido-telefone"];
+
+//endereço
+if (empty($_POST["pedido-endereco"])) {
+    $error .= "Endereço is required ";
+} else {
+    $endereco = $_POST["pedido-endereco"];
+}
+
+$quantidade = $_POST["pedido-quantidade"];
+$mensagem = $_POST["pedido-mensagem"];
+
+$cnpj = $_POST["pedido-cnpj"];
+$razao = $_POST["pedido-razao"];
+
+//endereço
+if (empty($_POST["pedido-endemp"])) {
+    $error .= "Endereço is required ";
+} else {
+    $endereco = $_POST["pedido-endemp"];
+}
+
+$insest = $_POST["pedido-insest"];
+
+//email_empresa
+if (empty($_POST["pedido-emailemp"])) {
+    $error .= "Email is required ";
+} else {
+    $email = $_POST["pedido-emailemp"];
+}
+
+$telemp = $_POST["pedido-telemp"];
+$simples = $_POST["pedido-simples"];
+ 
+$To = "colemais@eticketa.com.br";
+$uglySubject = "[Campanha | $assunto] Pedido formal";
+$Subject='=?UTF-8?B?'.base64_encode($uglySubject).'?=';
+
+$Body .= "
+<html>
+    <body style='width: 690px'>
+        <b style='text-transform: uppercase;'>DADOS DE CONTATO E ENTREGA</b><br/>
+        <b>Nome:</b> $nome<br/>
+        <b>Cargo / Empresa:</b> $empresa<br/>
+        <b>E-mail:</b> $email<br/>
+        <b>Telefone:</b> $telefone<br/>
+        <b>Endereço para entrega:</b> $endereco<br/>
+        <b>Quantidade (mínimo de 1.000):</b> $quantidade <br/>
+        <b>Observações / Dúvidas:</b> $mensagem<br/><br/>
+        <b style='text-transform: uppercase;'>DADOS PARA EMISSÃO DE BOLETO E NOTA FISCAL</b><br/>
+        <b>CNPJ:</b> $cnpj<br/>
+        <b>Razão Social:</b> $razao<br/>
+        <b>Endereço:</b> $endemp<br/>
+        <b>Inscrição Estatual:</b> $insest<br/>
+        <b>Email para envio da NF-e:</b> $emailemp<br/>
+        <b>Telefone:</b> $telemp<br/>
+        <b>Optante do Simples?</b> $simples      
+        
+    </body>
+</html>
+";
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-Transfer-Encoding: 8bit" . "\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8" . "\r\n";
+$headers .= "From: $email" . "\r\n";
+ 
+// send email
+$success = mail($To, $Subject, $Body, $headers);
+ 
+// redirect to success page
+if ($success && $error == ""){
+   echo "success";
+}else{
+    if($error == ""){
+        echo "Algo deu errado... Mas deu errado num nível, que é melhor você nos ligar no telefone (21) 3490-9292, porque pelo site vai ser difícil.";
+    } else {
+        echo $error;
+    }
+}
+ 
+?>
